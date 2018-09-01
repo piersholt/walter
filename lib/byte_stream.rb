3c14a7382a537ef420d05686b47cd0e487255f7e
Piers Holt <piersholt@gmail.com>
2018-09-01 17:19:32 +1000

    WIP: New abstract class ByteStream

---
+++ lib/byte_stream.rb
@@ -0,0 +1,55 @@
+# a byte collection
+class ByteStream
+  extend Forwardable
+
+  FORWARD_MESSAGES = %i[<< push first last each empty? size length count [] find_all reduce find find_index map group_by insert +].freeze
+
+  FORWARD_MESSAGES.each do |fwrd_message|
+    def_delegator :@bytes, fwrd_message
+  end
+
+  # def initialize(bytes = nil)
+  #   bytes.nil? ? super(0, nil) : super(bytes)
+  # end
+
+  def initialize(bytes = [])
+    @bytes = []
+  end
+
+  # def insert(*args)
+  #   @bytes.insert(*args)
+  # end
+
+  def to_s
+    map(&:h).join(' ').to_s
+  end
+
+  def +(other)
+    new_byte_strean = self.class.new
+    result = @bytes[0..-1] + other[0..-1]
+    new_byte_strean.wholesale(result)
+    new_byte_strean
+  end
+
+  def wholesale(new_bytes)
+    @bytes = new_bytes
+  end
+
+  # replaces Frame#header_s and Frame#tail_s
+  # def to_s
+  # end
+  #
+  # def inspect
+
+  # end
+
+  # def size
+  # end
+  #
+  # def length
+  # end
+  #
+  # def []
+  #   @bytes[]
+  # end
+end
