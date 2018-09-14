e4bb62e070ac495e2e1f6354a668a5a145f0eb34
Piers Holt <piersholt@gmail.com>
2018-09-15 09:42:08 +1000

    Add CCMDisplay#inspect

--- lib/commands/ccm_display.rb
+++ lib/commands/ccm_display.rb
@@ -38,6 +38,16 @@ class Commands
       # @chars = parse_chars(@argument_map[:chars])
     end
 
+    def inspect
+      # super handles from to command
+      # this only appends arguments
+      str_buffer = super
+      # TODO: this needs to be a string, not have Chars print itself
+      # puts @chars.overlay
+      # "[#{@arguments.map(&:h).join(' ')}]"
+
+      "#{h}\t#{@argument_map[:mode]} #{@argument_map[:control]}\t\"#{@text}\""
+    end
 
     def to_s
       return inspect if @verbose
