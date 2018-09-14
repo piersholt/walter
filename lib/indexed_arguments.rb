e4667ce47c593520f1f6dd05e12dc5e0b9c0620d
Piers Holt <piersholt@gmail.com>
2018-09-15 09:24:25 +1000

    Remove obsolete code from IndexedArguments

--- lib/indexed_arguments.rb
+++ lib/indexed_arguments.rb
@@ -2,12 +2,9 @@ class IndexedArguments < Bytes
   def initialize(bytes, index)
     super(bytes)
     @index = index
-
-    # create_readers
   end
 
   def lookup(name)
-    # binding.pry
     parameter_index = @index[name]
     d[parameter_index]
   end
@@ -15,10 +12,4 @@ class IndexedArguments < Bytes
   def parameters
     @index.keys
   end
-
-  private
-
-  # def create_readers
-  #   attr_reader(*parameters)
-  # end
 end
