77174a5088baf2fe12f4b39ed189d233732e48ec
Piers Holt <piersholt@gmail.com>
2018-09-14 14:19:17 +1000

    Clean up Key command

--- lib/commands/key.rb
+++ lib/commands/key.rb
@@ -1,23 +1,8 @@
 class Commands
-  # ID: 25 0x19
   class Key < BaseCommand
-    attr_accessor :v1, :v2
-
-    # SPEED_UNIT = 'kmph'.freeze
-    # REV_UNIT = 'rpm'.freeze
 
     def initialize(id, props)
       super(id, props)
-    end
-
-    # ---- Interface ---- #
-
-    def ambient
-      @v1
-    end
-
-    def coolant
-      @v2
     end
 
     # ---- Printable ---- #
@@ -30,14 +15,9 @@ class Commands
 
     # @override
     def to_s
-      # speed = "#{v1}#{SPEED_UNIT}"
-      # rev = "#{v2}#{REV_UNIT}"
-
       str_buffer = "#{sn}\t#{dict(:key, key)}: #{dict(:status, status)}"
       str_buffer
     end
-
-    private
 
   end
 end
