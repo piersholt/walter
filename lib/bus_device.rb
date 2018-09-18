6a1fe4d43a56cb9cfe72f3570f24c435b7413351
Piers Holt <piersholt@gmail.com>
2018-09-18 20:56:53 +1000

    Add new APIs to BusDevice

--- lib/bus_device.rb
+++ lib/bus_device.rb
@@ -1,7 +1,15 @@
 require 'api/key'
 require 'api/media'
+require 'api/ccm'
+require 'api/ike_sensors'
+require 'api/speed'
+require 'api/lamp'
 
 class BusDevice
   include API::Key
   include API::Media
+  include API::CCM
+  include API::IKESensors
+  include API::Speed
+  include API::Lamp
 end
