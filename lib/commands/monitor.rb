edf3fec71245cdaf8dca544b2d86c24ffb653701
Piers Holt <piersholt@gmail.com>
2018-09-20 10:41:35 +1000

    WIP: skeleton BMBTMonitor command class

---
+++ lib/commands/monitor.rb
@@ -0,0 +1,26 @@
+class Commands
+  class Monitor < BaseCommand
+
+    def initialize(id, props)
+      super(id, props)
+    end
+
+    # ---- Printable ---- #
+
+    def bytes
+      @bytes ||= {}
+    end
+
+    # ---- Core ---- #
+
+    # @override
+    def to_s
+      str_buffer = "#{sn}\tAction: #{action} (#{dictionary(:action)}), Nav: #{nav} (#{dictionary(:nav)})"
+      str_buffer
+    end
+
+    # def inspect
+    #   "#<#{self.class} @key=#{key} (#{dict(:key, key)}) @status=#{status} (#{dict(:status, status)})>"
+    # end
+  end
+end
