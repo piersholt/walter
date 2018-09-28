18cbf7242506ffb1af5ba2a814540cafbaed1835
Piers Holt <piersholt@gmail.com>
2018-09-28 11:18:32 +1000

    Remove obsolete ApplicationLayer

--- lib/walter.rb
+++ lib/walter.rb
@@ -14,7 +14,6 @@ require 'application/message'
 require 'physical/channel'
 require 'datalink/new_receiver'
 require 'datalink/transmitter'
-require 'application/application_layer'
 require 'listeners/global_listener'
 
 # Container
