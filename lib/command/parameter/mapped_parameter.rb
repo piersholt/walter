f2ea048521b2f47d9eaf3e2507df66903074b2c3
Piers Holt <piersholt@gmail.com>
2018-10-01 15:46:27 +1000

    Clean up MappedParameter

--- lib/command/parameter/mapped_parameter.rb
+++ lib/command/parameter/mapped_parameter.rb
@@ -12,36 +12,20 @@ class MappedParameter < BaseParameter
     "<#{PROC} @value=#{value}>"
   end
 
-  # @overide
   def to_s(width = DEFAULT_LABEL_WIDTH)
-    # LOGGER.info(PROC) { "#to_s(width = #{width})" }
-    # str_buffer = format("%-#{width}s", "#{label}#{LABEL_DELIMITER}")
     str_buffer = ""
-    # str_buffer = str_buffer.concat(LABEL_DELIMITER)
     str_buffer = str_buffer.concat("#{pretty}")
     str_buffer
   end
-
-  # def human_value
-  #   @human_value ||= @dictionary[value]
-  # end
 
   def pretty
     if @dictionary.nil?
       LOGGER.warn(PROC) { "Map @dictionary is nil!" }
       return "\"value\""
     elsif !@dictionary.key?(value)
-      LOGGER.warn(PROC) { "#{name}: #{d2h(value, true)} not found in @dictionary!" }
-      LOGGER.warn(PROC) { "#{name}: Dictionary: #{dictionary}" }
-      return "No states value!"
+      return "#{d2h(value, true)} not found!"
     else
       @dictionary[value]
     end
   end
-
-  # def try_dictionary(value)
-  #   return "NO DICTIONARY" if @dictionary.nil?
-  #   return "No dictionary value!" unless @dictionary.key?(value)
-  #   @dictionary[value]
-  # end
 end
