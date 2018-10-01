1e8257780b571008de1bbcce838b2d2a8c9cebb4
Piers Holt <piersholt@gmail.com>
2018-10-01 15:28:49 +1000

    Radio APi

--- lib/bus_device.rb
+++ lib/bus_device.rb
@@ -5,6 +5,8 @@ require 'api/ike_sensors'
 require 'api/speed'
 require 'api/lamp'
 require 'api/radio_led'
+require 'api/radio_gfx'
+
 
 class BusDevice
   include API::Key
@@ -14,4 +16,5 @@ class BusDevice
   include API::Speed
   include API::Lamp
   include API::RadioLED
+  include API::RadioGFX
 end
