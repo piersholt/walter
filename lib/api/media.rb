26860731f4a42e8669db992fac9913becc14d626
Piers Holt <piersholt@gmail.com>
2018-10-01 15:31:43 +1000

    Media API minor tweaks

--- lib/api/media.rb
+++ lib/api/media.rb
@@ -5,12 +5,14 @@ module API
     include BaseAPI
 
     COMMAND_ID = 0x23
-    TO_DEFAULT = Devices::GLOH
     FROM_DEFAULT = Devices::RAD
+    TO_DEFAULT = Devices::GLOH
 
-    def update(command_arguments, from_id = FROM_DEFAULT, to_id = TO_DEFAULT)
+
+    def media_display(command_arguments, from_id = FROM_DEFAULT, to_id = TO_DEFAULT)
+      # LOGGER.warn('API') { "#media_display(from_id=#{from_id}, to_id=#{to_id})" }
       command_id = COMMAND_ID
-      format_chars(command_arguments)
+      # format_chars(command_arguments)
       give_it_a_go(from_id, to_id, command_id, command_arguments)
     end
   end
