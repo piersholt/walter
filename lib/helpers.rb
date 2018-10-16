ee4640a89c49bed8d0b8437b69c1aada4f1da301
Piers Holt <piersholt@gmail.com>
2018-10-16 17:13:07 +1100

    Additional command aliases

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -22,12 +22,51 @@ module CommandAliases
   PING = 0x01
   PONG = 0x02
 
-  IGNITION = 0x11
+  IGNITION_REQ  = 0x10
+  IGNITION      = 0x11
+
+  SENSORS_REQ   = 0x12
+  SENSORS       = 0x13
+
+  CNTRY_REQ   = 0x14
+  CNTRY       = 0x15
+
+  ODO_REQ   = 0x16
+  ODO       = 0x17
+
+  SPEED     = 0x18
+  LAMP_REQ  = 0x5A
+  LAMP      = 0x5B
+
+  DOOR_REQ = 0x79
+  DOOR = 0x7A
+
+  VEH_REQ   = 0x53
+  VEH       = 0x54
+
+  TEMP_REQ = 0x1D
+  TEMP = 0x19
+
+  DSP_EQ = 0x34
+  RAD_EQ = 0x36
+
+  REMOTE    = 0x72
+  KEY_REQ   = 0x73
+  KEY       = 0x74
 
   TEL_3     = 0x20
   TEL_LED   = 0x2B
   TEL_STATE = 0x2C
   TEL_DATA  = 0x31
+
+  RAD_LED = 0x4A
+  SRC_CTL = 0x4B
+  SND_SRC = 0x4E
+
+  RAD_CONFIG = 0x45
+  RAD_STATUS = 0x46
+
+  GFX_STATUS = 0x22
 
   DSP_EQ = 0x34
 
