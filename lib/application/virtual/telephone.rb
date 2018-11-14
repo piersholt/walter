4e3dfcfe5ab9a7fb3b910d645ee3e24fedad17a1
Piers Holt <piersholt@gmail.com>
2018-11-14 16:15:17 +1100

    Telephone updates?

--- lib/application/virtual/telephone.rb
+++ lib/application/virtual/telephone.rb
@@ -6,23 +6,28 @@ module Telephone
   include API::Telephone
   include API::Media
 
-  # make all the contacts one character....
-  # i want to try and have to display more or less than two contacts per message
-  # and see what it does to render it
-  # if it donesn't just do... 2 at a time... then the idea of a four zone code is totally wrong...
-
-  # VALUES ------------------------------------------------------------------
-
-  LED_OFF = 0b0000_0000
-  LED_ALL = 0b0001_0101
-  TEL_OFF = 0b0000_0000
-  TEL_POWERED = 0b0001_0000
-
   PROC = 'SimTelephone'
 
-  # COMMANDS ------------------------------------------------------------------
-
   WAIT_DEFAULT = 0.01
+
+  CONTACTS = ['A.Anteater',
+              'B.Bonkers',
+              'C.Cockatoo',
+              'D.Donkey',
+              'E.Eagle',
+              'F.Fatty',
+              'G.Gonzo',
+              'H.Henderson',
+              'I.India',
+              'J.Jamie',
+              'K.Karen',
+              'L.Lorenzo',
+              'M.Mario',
+              'N.Nelly',
+              'O.Ophelia',
+              'P.Porridge'].freeze
+
+  # COMMANDS ------------------------------------------------------------------
 
   # PONG 0x02
   ANNOUNCE = 0x01
@@ -34,7 +39,7 @@ module Telephone
   CONTACT_DELIMITER = 6
 
   DRAW_DIRECTORY = 0x43
-  DRAW_TOP_8 = 0x80
+  DRAW_FAVOURITES = 0x80
 
   MID_DEFAULT = 0x01
 
@@ -45,7 +50,9 @@ module Telephone
 
   # GFX STATUS 0x22
   STATUS_CLEAR = 0x00
-  STATUS_GOOD = 0x03
+  STATUS_HOME_SUCCESS = 0x02
+  STATUS_SUCCESS = 0x03
+  STATUS_ERROR = 0xFF
 
   # TEL-DATA 0x31
   SOURCE_INFO = 0x20
@@ -53,57 +60,93 @@ module Telephone
   SOURCE_DIR = 0x43
   SOURCE_TOP = 0x80
 
+  ACTION_DIR_OPEN = 0x1F
   ACTION_DIR_BACK = 0x0C
   ACTION_DIR_FORWARD = 0x0D
 
-  # Handlers ------------------------------------------------------------------
+  # VALUES ------------------------------------------------------------------
 
-  def handle_tel_open(message)
-    LOGGER.unknown(PROC) { "Mock: handling telephone tel open request..." }
-    delegate_favourites(message)
+  LED_OFF = 0b0000_0000
+  LED_ALL = 0b0001_0101
+  TEL_OFF = 0b0000_0000
+  TEL_POWERED = 0b0001_0000
+
+  # Helpers  ------------------------------------------------------------------
+
+  def ready
+    LOGGER.unknown(PROC) { "Telephone ready macro!" }
+    tel_led(LED_OFF)
+    tel_state(TEL_OFF)
+    announce
+    sleep(0.25)
+    tel_state(TEL_POWERED)
+    tel_led(LED_ALL)
   end
 
-  # Piggyback off the radio announce to annunce
-  def handle_announce(message)
-    return false unless message.from?(:rad)
-    LOGGER.unknown(PROC) { "Mock: handling Announce!" }
-    return false unless message.command.status.value == ANNOUNCE
-    ready
-    true
+  # Displaying Drawing  -------------------------------------------------------
+
+  def clear(display)
+    LOGGER.unknown(PROC) { "Mock: clearing :#{display}!" }
+    command_arguments = {}
+
+    command_arguments[:m1] =
+      case display
+      when :directory
+        DRAW_DIRECTORY
+      when :favourites
+        DRAW_FAVOURITES
+      end
+
+    command_arguments[:m2]    = MID_DEFAULT
+    command_arguments[:m3]    = NO_PAGINATION
+    command_arguments[:chars] = CLEAR
+
+    mid(command_arguments, my_address, address(:gfx))
   end
 
-  def handle_data_request(message)
-    LOGGER.unknown(PROC) { "Mock: handling telephone data request..." }
-    # source = message.command.source
-    # case source
-    # when SOURCE_INFO
-    #   handle_info(message)
-    # when SOURCE_DIAL
-    #   handle_dial(message)
-    # when SOURCE_DIR
-    #   delegate_directory(message)
-    # when SOURCE_TOP
-    #   handle_top(message)
-    # end
+  def directory
+    contact_groups = generate_contacts(CONTACT_DISPLAY_LIMIT,
+                                       CONTACT_PAGE_SIZE,
+                                       false)
 
-    action = message.command.action.value
-    case action
-    when ACTION_DIR_BACK
-      delegate_directory(message)
-    when ACTION_DIR_FORWARD
-      delegate_directory(message)
+    contact_groups.each_with_index do |contact_group, i|
+      directory_page(contact_group, i)
     end
   end
 
-  def handle_gfx_status(message)
-    LOGGER.unknown(PROC) { "Mock: handling GFX status..." }
-    if message.command.status.value == STATUS_CLEAR
-      LOGGER.unknown(PROC) { "Mock: GFX status clear!" }
-      release_pending_render
-    elsif message.command.status.value == STATUS_GOOD
-      LOGGER.unknown(PROC) { "Mock: GFX status success!" }
-      clear_retry
+  def favourites
+    contact_groups = generate_contacts(CONTACT_DISPLAY_LIMIT,
+                                       CONTACT_PAGE_SIZE,
+                                       false)
+
+    contact_groups.each_with_index do |contact_group, i|
+      favourites_page(contact_group, i)
     end
+  end
+
+  # Helpers  ------------------------------------------------------------------
+
+  def generate_contacts(number_of_contacts = 1,
+                        contacts_per_group = CONTACT_PAGE_SIZE,
+                        inversed = false)
+    group_count = number_of_contacts.fdiv(contacts_per_group).ceil
+
+    Array.new(group_count) do |group_index|
+      full_char_array = Array.new(contacts_per_group) do |contact_index|
+        i = (group_index * contacts_per_group) + contact_index
+        contacts = generate_contact(i, inversed)
+        char_array = contacts.bytes
+        char_array.insert(0, CONTACT_DELIMITER)
+      end
+
+      full_char_array.flatten
+    end
+  end
+
+  def generate_contact(position = 1, inverse = false)
+    contacts = CONTACTS.dup
+    contacts.reverse! if inverse
+    contacts[position]
   end
 
   # Delegates  -----------------------------------------------------------------
@@ -116,6 +159,9 @@ module Telephone
     elsif message.command.action.value == ACTION_DIR_FORWARD
       LOGGER.unknown(PROC) { "Mock: oh, directory pages forward!" }
       render_pending_clearance(:directory)
+    elsif message.command.action.value == ACTION_DIR_OPEN
+      LOGGER.unknown(PROC) { "Mock: oh, directory pages forward!" }
+      render_pending_clearance(:directory)
     else
       false
     end
@@ -123,49 +169,38 @@ module Telephone
 
   def delegate_favourites(message)
     LOGGER.unknown(PROC) { "Mock: handling favourites..." }
+    LOGGER.unknown(PROC) { "Mock: oh, directory open!!" }
     render_pending_clearance(:favourites)
   end
 
-  # Transmission State  -----------------------------------------------------------------
+  # private
 
-  def render_pending_clearance(method)
-    LOGGER.unknown(PROC) { 'Mock: Cleaning Display' }
-    # clear
-    LOGGER.unknown(PROC) { "Mock: Now registering :#{method} as pending..." }
-    @render_pending = method
-    true
+  # xxx  -----------------------------------------------------------------
+
+  def directory_page(contact_group, i)
+    command_arguments = {}
+    command_arguments[:m1] = DRAW_DIRECTORY
+    command_arguments[:m2] = MID_DEFAULT
+    command_arguments[:m3] = PAGE[i]
+    command_arguments[:chars] = contact_group
+
+    mid(command_arguments, my_address, address(:gfx))
+    page_wait
   end
 
-  def release_pending_render
-    LOGGER.unknown(PROC) { "Mock: seeing is a render is pending...." }
-    if @render_pending.nil?
-      LOGGER.unknown(PROC) { 'Mock: No render pending...' }
-    else
-      LOGGER.unknown(PROC) { "Mock: releasing: :#{@render_pending}" }
-      # public_send(@render_pending)
-      @render_retry = @render_pending
-      @render_pending = nil
-    end
+  def favourites_page(contact_group, i)
+    command_arguments = {}
+    command_arguments[:m1] = DRAW_FAVOURITES
+    command_arguments[:m2] = MID_DEFAULT
+    command_arguments[:m3] = PAGE[i]
+    command_arguments[:chars] = contact_group
+
+    mid(command_arguments, my_address, address(:gfx))
+    page_wait
   end
 
-  def clear_retry
-    LOGGER.unknown(PROC) { "Mock: no need to try #{@render_retry} again!" }
-    @render_retry = nil
-  end
-
-  # Stateful  -----------------------------------------------------------------
-
-  DEFAULT_STATE = {
-    active: 0,
-    power: 0,
-    display: 0,
-    incoming: 0,
-    menu: 0,
-    hfs: 0
-  }.freeze
-
-  def default_state
-    DEFAULT_STATE.dup
+  def page_wait
+    Kernel.sleep(WAIT_DEFAULT)
   end
 
   # API calls  ----------------------------------------------------------------
@@ -189,122 +224,117 @@ module Telephone
     led({ leds: value }, my_address, to_id)
   end
 
-  # Helpers  ------------------------------------------------------------------
+  # Handlers ------------------------------------------------------------------
 
-  def ready
-    tel_led(LED_OFF)
-    tel_state(TEL_OFF)
-    announce
-    sleep(0.25)
-    tel_state(TEL_POWERED)
-    tel_led(LED_ALL)
+  def handle_tel_open(message)
+    LOGGER.unknown(PROC) { "Mock: handling telephone tel open request..." }
+    delegate_favourites(message)
   end
 
-  PARAMETER_LENGTH_MAX = 20
+  # Piggyback off the radio announce to annunce
+  def handle_announce(message)
+    # LOGGER.warn('SimulatedTEL') { "handling pong: #{message.from}" }
+    # LOGGER.warn('SimulatedTEL') { "message.from?(:rad) => #{message.from?(:rad)}" }
 
-  def clear
-    to_id = address(:gfx)
-
-    command_arguments = {}
-    command_arguments[:m1]    = DRAW_DIRECTORY
-    command_arguments[:m2]    = MID_DEFAULT
-    command_arguments[:m3]    = NO_PAGINATION
-    command_arguments[:chars] = CLEAR
-
-    mid(command_arguments, my_address, to_id)
+    return false unless message.from?(:rad)
+    return false unless message.command.status.value == ANNOUNCE
+    LOGGER.warn(PROC) { "Radio has announced. Piggybacking (sic)" }
+    ready
+    true
   end
 
-  def directory
-    to_id = address(:gfx)
-    contact_groups = generate_contacts(CONTACT_DISPLAY_LIMIT,
-                                       CONTACT_PAGE_SIZE,
-                                       false)
+  def handle_data_request(message)
+    LOGGER.unknown(PROC) { "Mock: handling telephone data request..." }
+    # source = message.command.source
+    # case source
+    # when SOURCE_INFO
+    #   handle_info(message)
+    # when SOURCE_DIAL
+    #   handle_dial(message)
+    # when SOURCE_DIR
+    #   directory(message)
+    # when SOURCE_TOP
+    #   handle_top(message)
+    # end
 
-    contact_groups.each_with_index do |contact_group, i|
-      directory_page(contact_group, i)
+    action = message.command.action.value
+    case action
+    when ACTION_DIR_OPEN
+      delegate_directory(message)
+    when ACTION_DIR_BACK
+      delegate_directory(message)
+    when ACTION_DIR_FORWARD
+      delegate_directory(message)
     end
   end
 
-  def favourites
-    to_id = address(:gfx)
-
-    contact_groups = generate_contacts(CONTACT_DISPLAY_LIMIT,
-                                       CONTACT_PAGE_SIZE,
-                                       false)
-
-    contact_groups.each_with_index do |contact_group, i|
-      favourites_page(contact_group, i)
+  def handle_gfx_status(message)
+    LOGGER.unknown(PROC) { "Mock: handling GFX status..." }
+    if message.command.status.value == STATUS_CLEAR
+      LOGGER.unknown(PROC) { "Mock: GFX status clear!" }
+      release_pending_render
+    elsif message.command.status.value == STATUS_SUCCESS
+      LOGGER.unknown(PROC) { "Mock: GFX status success!" }
+      clear_retry
+    elsif message.command.status.value == STATUS_HOME_SUCCESS
+      LOGGER.unknown(PROC) { "Mock: GFX menu status success!" }
+      clear_retry
+    elsif message.command.status.value == STATUS_ERROR
+      LOGGER.unknown(PROC) { "Mock: GFX status error!" }
+      release_pending_retry
     end
   end
 
-  def directory_page(contact_group, i)
-    command_arguments = {}
-    command_arguments[:m1] = DRAW_DIRECTORY
-    command_arguments[:m2] = MID_DEFAULT
-    command_arguments[:m3] = PAGE[i]
-    command_arguments[:chars] = contact_group
+  # Transmission State  -----------------------------------------------------------------
 
-    mid(command_arguments, my_address, to_id)
-    page_wait
+  def render_pending_clearance(method)
+    LOGGER.unknown(PROC) { "Mock: Being drawing :#{method}" }
+    # LOGGER.unknown(PROC) { "Mock: Firstly clear display :#{method}" }
+    clear(method)
+    LOGGER.unknown(PROC) { "Mock: Register :#{method} as pending..." }
+    @render_pending = method
+    true
   end
 
-  def favourites_page(contact_group, i)
-    command_arguments = {}
-    command_arguments[:m1] = DRAW_TOP_8
-    command_arguments[:m2] = MID_DEFAULT
-    command_arguments[:m3] = PAGE[i]
-    command_arguments[:chars] = contact_group
-
-    mid(command_arguments, my_address, to_id)
-    page_wait
-  end
-
-  def page_wait
-    Kernel.sleep(WAIT_DEFAULT)
-  end
-
-  # Helpers  ------------------------------------------------------------------
-
-  def generate_contacts(number_of_contacts = 1,
-                        contacts_per_group = CONTACT_PAGE_SIZE,
-                        inversed = false)
-    group_count = number_of_contacts.fdiv(contacts_per_group).ceil
-
-    Array.new(group_count) do |group_index|
-      full_char_array = Array.new(contacts_per_group) do |contact_index|
-        i = (group_index * contacts_per_group) + contact_index
-        contacts = generate_contact(i, inversed)
-        char_array = contacts.bytes
-        char_array.insert(0, CONTACT_DELIMITER)
-      end
-
-      full_char_array.flatten
+  def release_pending_retry
+    LOGGER.unknown(PROC) { "Mock: seeing is a render is pending...." }
+    if @render_retry.nil?
+      LOGGER.unknown(PROC) { 'Mock: No render retry......' }
+    else
+      LOGGER.unknown(PROC) { "Mock: retrying: :#{@render_retry}" }
+      public_send(@render_retry)
     end
   end
 
-  # Max contact name length is 13
-  # it will message two contacts in one message for a total of 28 bytes
+  def release_pending_render
+    LOGGER.unknown(PROC) { "Mock: seeing is a render is pending...." }
+    if @render_pending.nil?
+      LOGGER.unknown(PROC) { 'Mock: No render pending...' }
+    else
+      LOGGER.unknown(PROC) { "Mock: releasing: :#{@render_pending}" }
+      public_send(@render_pending)
+      @render_retry = @render_pending
+      @render_pending = nil
+    end
+  end
 
-  CONTACTS = ['A.Anteater',
-              'B.Bonkers',
-              'C.Cockatoo',
-              'D.Donkey',
-              'E.Eagle',
-              'F.Fatty',
-              'G.Gonzo',
-              'H.Henderson',
-              'I.India',
-              'J.Jamie',
-              'K.Karen',
-              'L.Lorenzo',
-              'M.Mario',
-              'N.Nelly',
-              'O.Ophelia',
-              'P.Porridge'].freeze
+  def clear_retry
+    # LOGGER.unknown(PROC) { "Mock: no need to try #{@render_retry} again!" }
+    @render_retry = nil
+  end
 
-  def generate_contact(position = 1, inverse = false)
-    contacts = CONTACTS.dup
-    contacts.reverse! if inverse
-    contacts[position]
+  # Stateful  -----------------------------------------------------------------
+
+  DEFAULT_STATE = {
+    active: 0,
+    power: 0,
+    display: 0,
+    incoming: 0,
+    menu: 0,
+    hfs: 0
+  }.freeze
+
+  def default_state
+    DEFAULT_STATE.dup
   end
 end
