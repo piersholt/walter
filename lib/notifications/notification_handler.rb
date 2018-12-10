a0c8baaf2882212c1690df1edc778f20ccdac879
Piers Holt <piersholt@gmail.com>
2018-12-11 08:36:43 +1100

    Update NotificationHandler to use Yabber::Serialized

--- lib/notifications/notification_handler.rb
+++ lib/notifications/notification_handler.rb
@@ -8,21 +8,29 @@ class NotificationHandler
 
   def oi(message)
     LOGGER.info(self.class) { message }
-    object = YAML.load(message)
-    LOGGER.info(self.class) { object.class }
-    LOGGER.info(self.class) { object.keys }
-    LOGGER.info(self.class) { object.values }
+    # object = YAML.load(message)
+    # LOGGER.info(self.class) { object.class }
+    # LOGGER.info(self.class) { object.keys }
+    # LOGGER.info(self.class) { object.values }
+    notification = Messaging::Serialized.new(message).parse
+    LOGGER.info(self.class) { "Deserialized: #{notification}" }
 
-    case object[:type]
+    case notification.topic
     when :device
-      device(object)
-    when :media
-      media(object)
+      device(notification)
+    when :target
+      # media(notification)
+    when :player
+      player(notification)
     end
+  rescue StandardError => e
+    LOGGER.error(PROC) { e }
+    e.backtrace.each { |l| LOGGER.error(l) }
+    binding.pry
   end
 
-  def device(object)
-    case object[:name]
+  def device(notification)
+    case notification.name
     when :device_connecting
       @bus.tel.bluetooth_connecting
     when :device_connected
@@ -32,14 +40,22 @@ class NotificationHandler
     when :device_disconnected
       @bus.tel.bluetooth_disconnected
     end
+  rescue StandardError => e
+    LOGGER.error(PROC) { e }
+    e.backtrace.each { |l| LOGGER.error(l) }
+    binding.pry
   end
 
-  def media(object)
-    case object[:name]
+  def player(notification)
+    case notification.name
     when :track_change
-      track = object[:options][:delta]['Track']
+      track = notification.properties
       LOGGER.fatal(self.class) { track }
       @bus.rad.track_change(track)
     end
+  rescue StandardError => e
+    LOGGER.error(PROC) { e }
+    e.backtrace.each { |l| LOGGER.error(l) }
+    binding.pry
   end
 end
