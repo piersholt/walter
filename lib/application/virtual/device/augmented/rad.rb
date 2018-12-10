bab8777fa9c3b0785fc608defcbc710c17195ca1
Piers Holt <piersholt@gmail.com>
2018-12-11 08:36:32 +1100

    Simplify AugmentedRadio track change behaviour

--- lib/application/virtual/device/augmented/rad.rb
+++ lib/application/virtual/device/augmented/rad.rb
@@ -57,31 +57,40 @@ class Virtual
 
     # ----------------------- AUGMENTED HANDLING --------------------- #
     def track_change(track)
-      return false if @thread
-      @thread = Thread.new(track) do |t|
-        begin
-          LOGGER.fatal(self.class) { t }
-          title = t['Title']
-          artist = t['Artist']
-          scroll = "#{title} / #{artist}"
-
-          displays( { chars: '', gfx: 0xC4, ike: 0x20,}, my_address, address(:ike) )
-
-          i = 0
-          last = scroll.length
-
-          while i <= last do
-            scroll = scroll.bytes.rotate(i).map { |i| i.chr }.join
-            displays( { chars: scroll, gfx: 0xC4, ike: 0x30,}, my_address, address(:ike) )
-            sleep(2)
-            i += 1
-          end
-        rescue StandardError => e
-          LOGGER.error(self.class) { e }
-        end
-      end
-      @thread = nil
-      true
+      title = track['Title']
+      artist = track['Artist']
+      output = "#{title} / #{artist}"
+      displays( { chars: output, gfx: 0xC4, ike: 0x30,}, my_address, address(:glo_h) )
+    rescue StandardError => e
+      LOGGER.error(self.class) { e }
     end
+
+    # def track_change(track)
+    #   return false if @thread
+    #   @thread = Thread.new(track) do |t|
+    #     begin
+    #       LOGGER.fatal(self.class) { t }
+    #       title = t['Title']
+    #       artist = t['Artist']
+    #       scroll = "#{title} / #{artist}"
+    #
+    #       displays( { chars: '', gfx: 0xC4, ike: 0x20,}, my_address, address(:ike) )
+    #
+    #       i = 0
+    #       last = scroll.length
+    #
+    #       while i <= last do
+    #         scroll = scroll.bytes.rotate(i).map { |i| i.chr }.join
+    #         displays( { chars: scroll, gfx: 0xC4, ike: 0x30,}, my_address, address(:ike) )
+    #         sleep(2)
+    #         i += 1
+    #       end
+    #     rescue StandardError => e
+    #       LOGGER.error(self.class) { e }
+    #     end
+    #   end
+    #   @thread = nil
+    #   true
+    # end
   end
 end
