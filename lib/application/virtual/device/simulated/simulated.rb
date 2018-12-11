88fd060713c5945749d95a977315040d3fe9c499
Piers Holt <piersholt@gmail.com>
2018-12-11 20:53:01 +1100

    Simulated Telephone

--- lib/application/virtual/device/simulated/simulated.rb
+++ lib/application/virtual/device/simulated/simulated.rb
@@ -2,11 +2,9 @@ root = 'application/virtual/device/simulated'
 
 require "#{root}/common/alive"
 
-# require "#{root}/mixins/cd"
-require "#{root}/mixins/telephone"
-
 require "#{root}/device"
 require "#{root}/cdc/changer_request"
 require "#{root}/cdc/cdc"
 require "#{root}/dsp"
-require "#{root}/tel"
+require "#{root}/tel/handlers"
+require "#{root}/tel/tel"
