19d78db174d89f7fe025ce2a1d3bf3277cdc61af
Piers Holt <piersholt@gmail.com>
2018-12-14 16:07:50 +1100

    Move AugmentedRadio handler from Actions module

--- lib/application/virtual/device/augmented/radio/actions.rb
+++ lib/application/virtual/device/augmented/radio/actions.rb
@@ -10,27 +10,6 @@ class Virtual
       include CommandGroups
       include ::Actions
 
-      def handle_message(message)
-        command_id = message.command.d
-        # return super if command_id == PING
-        LOGGER.debug(PROC) { "Handle? #{message.from} -> #{message.command.h}" }
-
-        case command_id
-        when MFL_FUNC
-          LOGGER.debug(PROC) { "Handling: MFL-FUNC" }
-          forward_back(message)
-        when BMBT_A
-          LOGGER.debug(PROC) { "BMBT-1 not implemented." }
-        when BMBT_B
-          LOGGER.debug(PROC) { "BMBT-2 not implemented." }
-        when SRC_CTL
-          LOGGER.debug(PROC) { "SRC_CTL not implemented." }
-        end
-      rescue StandardError => e
-        LOGGER.error(self.class) { e }
-        e.backtrace.each { |line| LOGGER.error(self.class) { line } }
-      end
-
       private
 
       def forward_back(message)
