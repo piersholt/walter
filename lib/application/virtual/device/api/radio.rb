2894488f1eb0d57245c2f48d027f92cbc3c38763
Piers Holt <piersholt@gmail.com>
2018-12-20 08:43:45 +1100

    Update Radio API for media display

--- lib/application/virtual/device/api/radio.rb
+++ lib/application/virtual/device/api/radio.rb
@@ -8,9 +8,13 @@ module API
     include CommandAliases
     include BaseAPI
 
+    # CD CHANGER
+
     def cd_changer_request(from: :rad, to: :cdc, arguments:)
       try(from, to, 0x38, arguments)
     end
+
+    # MENU
 
     def interface(from: :rad, to: :gfx, arguments:)
       try(from, to, 0x46, arguments)
@@ -18,6 +22,23 @@ module API
 
     def config(from: :gfx, to: :rad, arguments:)
       try(from, to, 0x45, arguments)
+    end
+
+    # DISPLAY
+
+    def output(from: :rad, to: :gfx, **arguments)
+      format_chars!(arguments)
+      try(from, to, 0x23, arguments)
+    end
+
+    def output_alt(from: :rad, to: :gfx, **arguments)
+      format_chars!(arguments)
+      try(from, to, 0xa5, arguments)
+    end
+
+    def index(from: :rad, to: :gfx, **arguments)
+      format_chars!(arguments)
+      try(from, to, 0x21, arguments)
     end
   end
 end
