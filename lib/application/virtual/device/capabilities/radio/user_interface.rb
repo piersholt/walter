ce58dd775adc7a985fcdb20ff2578308063bd489
Piers Holt <piersholt@gmail.com>
2018-12-21 08:23:59 +1100

    Additional Radio UI capabilities

--- lib/application/virtual/device/capabilities/radio/user_interface.rb
+++ lib/application/virtual/device/capabilities/radio/user_interface.rb
@@ -13,6 +13,25 @@ module Capabilities
       def overlay(option = 0b0000_1110)
         interface(arguments: { state: option })
       end
+
+      def manual
+        selection(mode: 0x40)
+      end
+
+      def eq(band = :eq)
+        case band
+        when :bass
+          selection(mode: 0xc0)
+        when :treble
+          selection(mode: 0xd0)
+        when :fader
+          selection(mode: 0xe0)
+        when :balance
+          selection(mode: 0xf0)
+        when :eq
+          selection(mode: 0x80)
+        end
+      end
     end
   end
 end
