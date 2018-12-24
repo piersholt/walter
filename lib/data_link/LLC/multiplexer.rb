ebfbeecf2a1cd4bb89404cfb76d98af068c50c3f
Piers Holt <piersholt@gmail.com>
2018-12-24 16:26:20 +1100

    Multiplexer logger

--- lib/data_link/LLC/multiplexer.rb
+++ lib/data_link/LLC/multiplexer.rb
@@ -38,37 +38,37 @@ module DataLink
       alias proc_name name
 
       def off
-        LogActually.datalink.debug(name) { "#{self.class}#off" }
+        LogActually.multiplexer.debug(name) { "#{self.class}#off" }
         close_threads
       end
 
       def on
-        LogActually.datalink.debug(name) { "#{self.class}#on" }
+        LogActually.multiplexer.debug(name) { "#{self.class}#on" }
         @write_thread = thread_write_output_frame_buffer(@frame_output_buffer, @packet_output_buffer)
         @threads.add(@write_thread)
         true
       rescue StandardError => e
-        LogActually.datalink.error(e)
-        e.backtrace.each { |l| LogActually.datalink.error(l) }
+        LogActually.multiplexer.error(e)
+        e.backtrace.each { |l| LogActually.multiplexer.error(l) }
         raise e
       end
 
       def thread_write_output_frame_buffer(frame_output_buffer, packet_output_buffer)
-        LogActually.datalink.debug(name) { 'New Thread: Frame Multiplexing' }
+        LogActually.multiplexer.debug(name) { 'New Thread: Frame Multiplexing' }
         Thread.new do
           Thread.current[:name] = name
           begin
             loop do
               message = packet_output_buffer.pop
               new_frame = multiplex(message)
-              LogActually.datalink.debug(name) { "frame_output_buffer.push(#{new_frame})" }
+              LogActually.multiplexer.debug(name) { "frame_output_buffer.push(#{new_frame})" }
               frame_output_buffer.push(new_frame)
             end
           rescue StandardError => e
-            LogActually.datalink.error(name) { e }
-            e.backtrace.each { |l| LogActually.datalink.error(l) }
+            LogActually.multiplexer.error(name) { e }
+            e.backtrace.each { |l| LogActually.multiplexer.error(l) }
           end
-          LogActually.datalink.warn(name) { "End Thread: Frame Multiplexing" }
+          LogActually.multiplexer.warn(name) { "End Thread: Frame Multiplexing" }
         end
       end
 
@@ -76,7 +76,7 @@ module DataLink
 
       # @return Frame
       def multiplex(message)
-        LogActually.datalink.debug(name) { "#multiplex(#{message})" }
+        LogActually.multiplexer.debug(name) { "#multiplex(#{message})" }
         frame_builder = FrameBuilder.new
 
         frame_builder.from = message.from.d
@@ -84,7 +84,7 @@ module DataLink
         frame_builder.command = message.command
 
         frame = frame_builder.result
-        LogActually.datalink.unknown(name) { "Frame build: #{frame.h}" }
+        LogActually.multiplexer.debug(name) { "Frame build: [#{frame.h.join(' ')}]" }
         frame
       end
     end
