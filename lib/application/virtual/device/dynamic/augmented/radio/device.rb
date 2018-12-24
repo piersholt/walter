b9ddffc6c59f4f427c89ce96aead52cd07a7b6e5
Piers Holt <piersholt@gmail.com>
2018-12-24 16:29:43 +1100

    Radio device logger

--- lib/application/virtual/device/dynamic/augmented/radio/device.rb
+++ lib/application/virtual/device/dynamic/augmented/radio/device.rb
@@ -16,22 +16,22 @@ class Virtual
     def handle_message(message)
       command_id = message.command.d
       # return super if command_id == PING
-      LOGGER.debug(PROC) { "Handle? #{message.from} -> #{message.command.h}" }
+      LogActually.rad.debug(PROC) { "Handle? #{message.from} -> #{message.command.h}" }
 
       case command_id
       when MFL_FUNC
-        LOGGER.debug(PROC) { "Handling: MFL-FUNC" }
+        LogActually.rad.debug(PROC) { "Handling: MFL-FUNC" }
         forward_back(message)
       when BMBT_A
-        LOGGER.debug(PROC) { "BMBT-1 not implemented." }
+        LogActually.rad.debug(PROC) { "BMBT-1 not implemented." }
       when BMBT_B
-        LOGGER.debug(PROC) { "BMBT-2 not implemented." }
+        LogActually.rad.debug(PROC) { "BMBT-2 not implemented." }
       when SRC_CTL
-        LOGGER.debug(PROC) { "SRC_CTL not implemented." }
+        LogActually.rad.debug(PROC) { "SRC_CTL not implemented." }
       end
     rescue StandardError => e
-      LOGGER.error(self.class) { e }
-      e.backtrace.each { |line| LOGGER.error(self.class) { line } }
+      LogActually.rad.error(self.class) { e }
+      e.backtrace.each { |line| LogActually.rad.error(self.class) { line } }
     end
 
   end
