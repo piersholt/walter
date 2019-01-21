ee04aa9f9a2ac78f77cc1c1b4b04068ac267a242
Piers Holt <piersholt@gmail.com>
2019-01-22 09:23:10 +1100

    Update Stageful module to support merging nested hash arguments

--- lib/helpers/stateful.rb
+++ lib/helpers/stateful.rb
@@ -6,7 +6,13 @@ module Stateful
   private
 
   def state!(delta)
-    state.merge!(delta)
+    state.merge!(delta) do |_, val_old, val_new|
+      if val_old.is_a?(Hash) && val_new.is_a?(Hash)
+        val_old.merge(val_new)
+      else
+        val_new
+      end
+    end
   end
 
   def start_timer
