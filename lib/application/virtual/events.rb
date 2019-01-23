c5b346b4357ca2635370d38777a45f7ce5a51fed
Piers Holt <piersholt@gmail.com>
2019-01-24 10:01:45 +1100

    Virtual Display events

--- lib/application/virtual/events.rb
+++ lib/application/virtual/events.rb
@@ -8,5 +8,18 @@ class Virtual
     RADIO_LAYOUT_TRAFFIC = :radio_layout_traffic
     RADIO_LAYOUT_CDC = :radio_layout_cdc
     RADIO_LAYOUT_UNKNOWN = :radio_layout_unknown
+
+    GFX_IDLE = :gfx_priority_gfx
+    GFX_BUSY  = :gfx_priority_radio
+
+    GFX_MONITOR_ON  = :gfx_monitor_on
+    GFX_MONITOR_OFF = :gfx_monitor_off
+
+    INPUT_CONFIRM_SELECT = :input_confirm_select
+    INPUT_CONFIRM_HOLD = :input_confirm_hold
+    INPUT_CONFIRM_RELEASE = :input_confirm_release
+
+    INPUT_LEFT = :input_left
+    INPUT_RIGHT = :input_right
   end
 end
