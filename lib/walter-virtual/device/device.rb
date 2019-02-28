461c9b0b3d4f21f7c0ae5fdd7a8a6c3d825d8765
Piers Holt <piersholt@gmail.com>
2019-02-28 17:00:08 +1100

    Update paths

---
+++ lib/walter-virtual/device/device.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+puts "\tLoading walter-virtual/device"
+
+device_root = 'device'
+require "#{device_root}/events"
+require "#{device_root}/receivable"
+require "#{device_root}/base"
+
+puts "\tLoading walter-virtual/device/dynamic"
+
+require "#{device_root}/api/api"
+require "#{device_root}/capabilities/capabilities"
+require "#{device_root}/device_builder"
+require "#{device_root}/dynamic"
+
+puts "\tLoading walter-virtual/device/augmented"
+
+augmented_root = 'device/augmented'
+
+require "#{augmented_root}/device"
+
+# Radio
+radio_root = augmented_root + '/radio'
+require "#{radio_root}/state/constants"
+require "#{radio_root}/state/model"
+require "#{radio_root}/state/chainable"
+require "#{radio_root}/state"
+require "#{radio_root}/actions"
+require "#{radio_root}/notifications"
+require "#{radio_root}/device"
+
+# Radio
+gfx_root = augmented_root + '/gfx'
+require "#{gfx_root}/state/constants"
+require "#{gfx_root}/state/model"
+require "#{gfx_root}/state/chainable"
+require "#{gfx_root}/state/sent"
+require "#{gfx_root}/state/received"
+require "#{gfx_root}/state"
+require "#{gfx_root}/device"
+
+# BMBT
+bmbt_root = augmented_root + '/bmbt'
+# require "#{bmbt_root}/state/constants"
+# require "#{bmbt_root}/state/model"
+# require "#{bmbt_root}/state/chainable"
+require "#{bmbt_root}/state/sent"
+# require "#{bmbt_root}/state/received"
+require "#{bmbt_root}/state"
+require "#{bmbt_root}/device"
+
+puts "\tLoading walter-virtual/device/emulated"
+
+emualted_root = 'device/emulated'
+
+require "#{emualted_root}/common/alive"
+
+require "#{emualted_root}/device"
+
+require "#{emualted_root}/cdc/handlers"
+require "#{emualted_root}/cdc/device"
+
+require "#{emualted_root}/dsp/device"
+
+require "#{emualted_root}/rad/device"
+
+require "#{emualted_root}/tel/handlers"
+require "#{emualted_root}/tel/device"
+
+require "#{emualted_root}/diagnostics/device"
+
+require "#{emualted_root}/dummy/device"
+
+puts "\tLoading walter-virtual/bus"
--- lib/walter-virtual/walter-virtual.rb
+++ lib/walter-virtual/walter-virtual.rb
@@ -2,24 +2,7 @@
 
 puts 'Loading walter-virtual'
 
-puts "\tLoading walter-virtual/device"
-
-device_root = 'device'
-require "#{device_root}/events"
-require "#{device_root}/receivable"
-require "#{device_root}/base"
-
-puts "\tLoading walter-virtual/device/dynamic"
-
-require "#{device_root}/api/api"
-require "#{device_root}/capabilities/capabilities"
-require "#{device_root}/device_builder"
-require "#{device_root}/dynamic"
-
-require "#{device_root}/augmented/augmented"
-require "#{device_root}/emulated/emulated"
-
-puts "\tLoading walter-virtual/bus"
+require 'device/device'
 
 bus_root = 'bus'
 require "#{bus_root}/devices"
