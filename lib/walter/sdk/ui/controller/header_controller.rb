789afa5da8a5f7ec43338f1021980d3a9a008350
Piers Holt <piersholt@gmail.com>
2019-06-07 11:39:46 +1000

    Clean up HeaderController

--- lib/walter/sdk/ui/controller/header_controller.rb
+++ lib/walter/sdk/ui/controller/header_controller.rb
@@ -5,13 +5,7 @@ module Wolfgang
     module Controller
       # Comment
       class HeaderController < BaseController
-        NAME = 'HeaderController'
-        attr_reader :addressed_player
-        alias player addressed_player
-
-        # def initialize(context)
-        #   super(context)
-        # end
+        NAME = 'HeaderController'.freeze
 
         def name
           NAME
@@ -22,23 +16,18 @@ module Wolfgang
         def header
           LogActually.ui.unknown(NAME) { '#header' }
           view = View::Header::Status.new
-          # view = View::Header::S.new(addressed_player)
           render_header(view)
         end
 
         # Setup ------------------------------------------------------
 
         def create(view, selected_menu_item: nil)
-          LogActually.ui.debug('HeaderController') { "#create(#{view})" }
+          LogActually.ui.debug(NAME) { "#create(#{view})" }
           case view
           when :header
-            # return true if addressed_player
-            # @addressed_player.delete_observer(self) rescue nil
-            # @addressed_player = context.audio.player
-            # addressed_player.add_observer(self, :player_update)
             true
           else
-            # LogActually.ui.warn(NAME) { "Create: #{view} view not recognised." }
+            LogActually.ui.warn(NAME) { "Create: #{view} view not recognised." }
             false
           end
         end
@@ -46,28 +35,17 @@ module Wolfgang
         def destroy(view)
           case view
           when :header
-            @addressed_player.delete_observer(self)
-            @addressed_player = nil
             true
           else
-            # LogActually.ui.warn(NAME) { "Destroy: #{view} view not recognised." }
+            LogActually.ui.warn(NAME) { "Destroy: #{view} view not recognised." }
             false
           end
         end
 
         # USER EVENTS ------------------------------------------------------
-
+        # none
         # DATA EVENTS ------------------------------------------------------
-
-        def player_update(action, player:)
-          LogActually.ui.debug('HeaderController') { "#player_update(#{action}, #{player})" }
-          case action
-          when :track_change
-            header
-          when :status
-            header
-          end
-        end
+        # none
       end
     end
   end
