2c3ce0b4c356734cf7767a02e0946f8163c2f0c6
Piers Holt <piersholt@gmail.com>
2019-06-07 10:27:25 +1000

    Clean up walter-api requires

--- lib/walter/api.rb
+++ lib/walter/api.rb
@@ -4,12 +4,13 @@ puts 'Loading walter/api'
 
 # Walter-API
 LogActually.is_all_around(:display)
-LogActually.display.w
 LogActually.is_all_around(:controls)
-LogActually.controls.i
 LogActually.is_all_around(:audio)
-LogActually.audio.i
 LogActually.is_all_around(:tel)
+
+LogActually.display.w
+LogActually.controls.i
+LogActually.audio.i
 LogActually.tel.i
 
 puts "\tLoading walter/api/display"
@@ -37,18 +38,6 @@ require "#{display_root}/display"
 
 puts "\tLoading walter/api/controls"
 
-# controls_root = 'controls'
-# require "#{controls_root}/states/defaults"
-# require "#{controls_root}/states/unknown"
-# require "#{controls_root}/states/disabled"
-# require "#{controls_root}/states/enabled"
-# require "#{controls_root}/states/busy"
-# require "#{controls_root}/states/available"
-# require "#{controls_root}/states/captured"
-# require "#{controls_root}/states/overwritten"
-
-# require "#{controls_root}/handlers/cache_handler"
-# require "#{controls_root}/handlers/input_handler"
 require_relative 'api/controls/listener'
 require_relative 'api/controls/control/base'
 require_relative 'api/controls/control/stateless'
@@ -61,36 +50,12 @@ require_relative 'api/controls/controls'
 puts "\tLoading walter/api/audio"
 
 audio_root = 'audio'
-# require "#{audio_root}/states/defaults"
-# require "#{audio_root}/states/unknown"
-# require "#{audio_root}/states/disabled"
-# require "#{audio_root}/states/enabled"
-# require "#{audio_root}/states/busy"
-# require "#{audio_root}/states/available"
-# require "#{audio_root}/states/captured"
-# require "#{audio_root}/states/overwritten"
-
-# require "#{audio_root}/handlers/cache_handler"
-# require "#{audio_root}/handlers/input_handler"
-# require "#{audio_root}/listener"
 
 require "#{audio_root}/audio"
 
 puts "\tLoading walter/api/telephone"
 
 telephone_root = 'telephone'
-# require "#{audio_root}/states/defaults"
-# require "#{audio_root}/states/unknown"
-# require "#{audio_root}/states/disabled"
-# require "#{audio_root}/states/enabled"
-# require "#{audio_root}/states/busy"
-# require "#{audio_root}/states/available"
-# require "#{audio_root}/states/captured"
-# require "#{audio_root}/states/overwritten"
-
-# require "#{audio_root}/handlers/cache_handler"
-# require "#{audio_root}/handlers/input_handler"
-# require "#{audio_root}/listener"
 
 require "#{telephone_root}/telephone"
 
