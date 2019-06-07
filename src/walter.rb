1ad20ae9ca754cc9f36f8ea68972a36447341c5c
Piers Holt <piersholt@gmail.com>
2019-06-07 10:02:25 +1000

    Remove service instance variables from driver class

--- src/walter.rb
+++ src/walter.rb
@@ -200,13 +200,13 @@ class Walter
   def setup_sdk
     @wolfgang = Wolfgang::ApplicationContext.new
 
-    @manager = Wolfgang::Manager.new
-    @audio = Wolfgang::Audio.new
+    manager = Wolfgang::Manager.new
+    audio = Wolfgang::Audio.new
 
-    @manager.disable
-    @audio.disable
+    manager.disable
+    audio.disable
 
-    @wolfgang.register_service(:manager, @manager)
-    @wolfgang.register_service(:audio, @audio)
+    @wolfgang.register_service(:manager, manager)
+    @wolfgang.register_service(:audio, audio)
   end
 end
