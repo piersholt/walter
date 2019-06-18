77173433375fa1038f06efd27c82234aa62a5c3a
Piers Holt <piersholt@gmail.com>
2019-06-18 18:30:44 +1000

    Update wilhelm-services/ui load path logging

--- lib/wilhelm/services/ui.rb
+++ lib/wilhelm/services/ui.rb
@@ -5,10 +5,10 @@ root = 'ui'
 # UI
 ui_root = root
 
-puts "\tLoading walter/ui/view"
+puts "\tLoading wilhelm/services/ui/view"
 
 require_relative 'ui/view'
 
-puts "\tLoading walter/ui/controller"
+puts "\tLoading wilhelm/services/ui/controller"
 
 require_relative 'ui/controller'
