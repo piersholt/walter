353680204abd42713c7e2c559ad87b943f18ce9e
Piers Holt <piersholt@gmail.com>
2019-06-19 14:37:01 +1000

    Update namespacing

--- lib/wilhelm/virtual/bus/initialization.rb
+++ lib/wilhelm/virtual/bus/initialization.rb
@@ -1,69 +1,69 @@
-
-class Wilhelm::Virtual
-  class Initialization
-    PROC = 'Initialization'
-    def initialize(emulated: [], augmented: [])
-      @emulated = emulated
-      @augmented = augmented
-      @executed = false
-    end
-
-    def execute
-      bus = create_bus
-      device_idents = lookup_devices
-      devices = create_devices(device_idents)
-      populate_bus(bus, devices)
-      bus
-    end
-
-    def create_bus
-      Bus.instance
-    end
-
-    def lookup_devices
-      alt = Wilhelm::Core::AddressLookupTable.instance
-      idents = alt.idents
-      LogActually.virtual.debug(PROC) { "Idents: #{idents}|" }
-      idents
-    end
-
-    def create_devices(device_idents)
-      device_idents.map do |device_ident|
-        create_device(device_ident)
+module Wilhelm
+  class Virtual
+    class Initialization
+      PROC = 'Initialization'
+      def initialize(emulated: [], augmented: [])
+        @emulated = emulated
+        @augmented = augmented
+        @executed = false
       end
-    end
 
-    def create_device(device_ident)
-      if @emulated.include?(device_ident)
-        LogActually.virtual.debug(PROC) { "Create Emulated #{device_ident}" }
-        DynamicDevice.builder
-                     .target(device_ident)
-                     .result
-      elsif @augmented.include?(device_ident)
-        LogActually.virtual.debug(PROC) { "Create Augmented #{device_ident}" }
-        augmented_device =
+      def execute
+        bus = create_bus
+        device_idents = lookup_devices
+        devices = create_devices(device_idents)
+        populate_bus(bus, devices)
+        bus
+      end
+
+      def create_bus
+        Bus.instance
+      end
+
+      def lookup_devices
+        idents = Core::AddressLookupTable.instance.idents
+        LogActually.virtual.debug(PROC) { "Idents: #{idents}|" }
+        idents
+      end
+
+      def create_devices(device_idents)
+        device_idents.map do |device_ident|
+          create_device(device_ident)
+        end
+      end
+
+      def create_device(device_ident)
+        if @emulated.include?(device_ident)
+          LogActually.virtual.debug(PROC) { "Create Emulated #{device_ident}" }
           DynamicDevice.builder
-                       .target(device_ident)
-                       .result
-        # augmented_device.add_observer(intent_listener, :handle)
-        augmented_device
-      else
-        LogActually.virtual.debug(PROC) { "Create dumb: #{device_ident}|" }
-        Device.new(device_ident)
-      end
-    end
-
-    # def intent_listener
-    #   @intent_listener ||= IntentListener.instance
-    # end
-
-    def populate_bus(bus, devices)
-      devices.each do |device|
-        bus.add_device(device)
+          .target(device_ident)
+          .result
+        elsif @augmented.include?(device_ident)
+          LogActually.virtual.debug(PROC) { "Create Augmented #{device_ident}" }
+          augmented_device =
+          DynamicDevice.builder
+          .target(device_ident)
+          .result
+          # augmented_device.add_observer(intent_listener, :handle)
+          augmented_device
+        else
+          LogActually.virtual.debug(PROC) { "Create dumb: #{device_ident}|" }
+          Device.new(device_ident)
+        end
       end
 
-      LogActually.virtual.debug(PROC) { "Bus Devices: #{bus.devices}" }
-      true
+      # def intent_listener
+      #   @intent_listener ||= IntentListener.instance
+      # end
+
+      def populate_bus(bus, devices)
+        devices.each do |device|
+          bus.add_device(device)
+        end
+
+        LogActually.virtual.debug(PROC) { "Bus Devices: #{bus.devices}" }
+        true
+      end
     end
   end
 end
