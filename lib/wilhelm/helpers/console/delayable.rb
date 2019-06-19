7ffc9b8d0fb011603327075ff0373919e6c7341a
Piers Holt <piersholt@gmail.com>
2019-06-19 13:32:01 +1000

    wilhelm-helpers/console missing variable

--- lib/wilhelm/helpers/console/delayable.rb
+++ lib/wilhelm/helpers/console/delayable.rb
@@ -6,26 +6,30 @@ module Wilhelm
     # Helpers for common tasks on CLI
     module Console
       module Delayable
+        def interface_read_thread
+          @core.interface.read_thread
+        end
+
         def nap(seconds)
-          # @interface.read_thread[:sleep_time] = seconds
-          @interface.read_thread.thread_variable_set(:sleep_time, seconds)
+          # interface_read_thread[:sleep_time] = seconds
+          interface_read_thread.thread_variable_set(:sleep_time, seconds)
         end
 
         def sleepy?
-          # @interface.read_thread[:sleep_enabled] = false
-          @interface.read_thread.thread_variable_get(:sleep_enabled)
+          # interface_read_thread[:sleep_enabled] = false
+          interface_read_thread.thread_variable_get(:sleep_enabled)
         end
 
         def awake!
-          t = @interface.read_thread
-          # @interface.read_thread[:sleep_enabled] = false
+          t = interface_read_thread
+          # interface_read_thread[:sleep_enabled] = false
           t.thread_variable_set(:sleep_enabled, false)
           # puts "[#{t[:name]}] :sleep_enabled => #{sleepy?}"
         end
 
         def sleepy!
-          t = @interface.read_thread
-          # @interface.read_thread[:sleep_enabled] = true
+          t = interface_read_thread
+          # interface_read_thread[:sleep_enabled] = true
           t.thread_variable_set(:sleep_enabled, true)
           # puts "[#{t[:name]}] :sleep_enabled => #{sleepy?}"
         end
