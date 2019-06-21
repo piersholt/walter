637148e2110fad8cd0fb71bc376fd53e8991c2b9
Piers Holt <piersholt@gmail.com>
2019-06-21 21:17:35 +1000

    Remove obsolete Broadcast device type

--- lib/wilhelm/virtual/device.rb
+++ lib/wilhelm/virtual/device.rb
@@ -4,8 +4,6 @@ puts "\tLoading wilhelm/virtual/device"
 
 require_relative 'device/receivable'
 require_relative 'device/base'
-# TODO: remove broadcast
-require_relative 'device/broadcast'
 
 require_relative 'device/dynamic'
 require_relative 'device/augmented'
