df9291d7f0128f9579c6bcae3525453d6e468efd
Piers Holt <piersholt@gmail.com>
2019-06-21 14:06:02 +1000

    Nest wilhelm-core/listener/core under Listener module

--- lib/wilhelm/core/listener/core_listener.rb
+++ lib/wilhelm/core/listener/core_listener.rb
@@ -2,34 +2,36 @@
 
 module Wilhelm
   module Core
-    # Comment
-    class CoreListener < BaseListener
-      attr_accessor :interface_handler
+    module Listener
+      # Comment
+      class CoreListener < BaseListener
+        attr_accessor :interface_handler
 
-      def initialize(interface_handler: nil)
-        @interface_handler = interface_handler
-      end
-
-      NAME = 'Core::CoreListener'
-
-      def name
-        NAME
-      end
-
-      def update(___, ___ = {})
-        case action
-        when BYTE_RECEIVED
-          false
-        when FRAME_RECEIVED
-          false
-        when BUS_ONLINE
-          false
-        when BUS_OFFLINE
-          interface_handler&.bus_offline
+        def initialize(interface_handler: nil)
+          @interface_handler = interface_handler
         end
-      rescue StandardError => e
-        LOGGER.error(name) { e }
-        e.backtrace.each { |l| LOGGER.error(l) }
+
+        NAME = 'Core::CoreListener'
+
+        def name
+          NAME
+        end
+
+        def update(action, ___ = {})
+          case action
+          when BYTE_RECEIVED
+            false
+          when FRAME_RECEIVED
+            false
+          when BUS_ONLINE
+            false
+          when BUS_OFFLINE
+            interface_handler&.bus_offline
+          end
+        rescue StandardError => e
+          LOGGER.error(name) { e }
+          e.backtrace.each { |l| LOGGER.error(l) }
+        end
       end
     end
   end
