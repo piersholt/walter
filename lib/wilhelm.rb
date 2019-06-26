7bcbc016a9c692ab94c505ae1cc99860c1eead5f
Piers Holt <piersholt@gmail.com>
2019-06-26 14:54:52 +1000

    Update bin/run global load path setup

--- lib/wilhelm.rb
+++ lib/wilhelm.rb
@@ -1,32 +1,16 @@
 # frozen_string_literal: true
 
 begin
-  def add_load_path(dir)
-    load_path = File.expand_path(File.dirname(__FILE__) + "/wilhelm/#{dir}")
-    $LOAD_PATH.unshift(load_path) unless $LOAD_PATH.include?(load_path)
-  rescue StandardError => e
-    puts e
-    e.backtrace.each { |line| puts line }
-    exit
-  end
-
-  add_load_path('helpers')
-  add_load_path('core')
-  add_load_path('virtual')
-  add_load_path('api')
-  add_load_path('sdk')
-  add_load_path('services')
-
   # Setup namespace prior to loading deprendencies to allow for any files that
   # reference Wilhelm via short names, i.e. class Wilhelm::MyKlass
   module Wilhelm; end
 
-  require 'wilhelm/helpers'
-  require 'wilhelm/core'
-  require 'wilhelm/virtual'
-  require 'wilhelm/api'
-  require 'wilhelm/sdk'
-  require 'wilhelm/services'
+  require_relative 'wilhelm/helpers'
+  require_relative 'wilhelm/core'
+  require_relative 'wilhelm/virtual'
+  require_relative 'wilhelm/api'
+  require_relative 'wilhelm/sdk'
+  require_relative 'wilhelm/services'
 rescue LoadError => e
   puts "#{e.class}: #{e}"
   e.backtrace.each { |line| puts "\t#{line}" }
