32a495e1db59cacf41c8e4bb1f632fffbf1fabc9
Piers Holt <piersholt@gmail.com>
2019-06-29 09:44:50 +1000

    Clean up indentation

--- lib/wilhelm/virtual/protocol/maps/command_map.rb
+++ lib/wilhelm/virtual/protocol/maps/command_map.rb
@@ -26,32 +26,33 @@ module Wilhelm
           mapped_result = find(command_id)
         rescue IndexError
           LOGGER.error(PROC) {
-            "Command ID: #{command_id}, #{Helpers::DataTools.d2h(command_id, true)} not found!" }
-            mapped_result = find(:default)
-            mapped_result[:id][:d] = command_id
-          end
-
-          if from
-            schemas = mapped_result[:schemas]
-            if schemas
-              # puts 'breakable point'
-              has_schema = schemas.include?(from)
-            end
-            if has_schema
-              # puts 'breakable point'
-              mapped_result = mapped_result[from]
-            end
-          end
-
-          mapped_result[:default_id] = command_id
-
-          command_configuration = CommandConfiguration.new(mapped_result)
-          command_configuration
+            "Command ID: #{command_id}, #{Helpers::DataTools.d2h(command_id, true)} not found!"
+          }
+          mapped_result = find(:default)
+          mapped_result[:id][:d] = command_id
         end
 
-        def config(command_id, from = nil)
-          find_or_default(command_id, from)
+        if from
+          schemas = mapped_result[:schemas]
+          if schemas
+            # puts 'breakable point'
+            has_schema = schemas.include?(from)
+          end
+          if has_schema
+            # puts 'breakable point'
+            mapped_result = mapped_result[from]
+          end
         end
+
+        mapped_result[:default_id] = command_id
+
+        command_configuration = CommandConfiguration.new(mapped_result)
+        command_configuration
       end
+
+      def config(command_id, from = nil)
+        find_or_default(command_id, from)
+      end
+    end
   end
 end
