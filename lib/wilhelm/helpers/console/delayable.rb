aec28a2679c0fa83e146a13cd7547845e5c03cf0
Piers Holt <piersholt@gmail.com>
2019-07-27 12:07:14 +1000

    wilhelm-helpes: Delayable remove obsolete source

--- lib/wilhelm/helpers/console/delayable.rb
+++ lib/wilhelm/helpers/console/delayable.rb
@@ -10,27 +10,21 @@ module Wilhelm
         end
 
         def nap(seconds)
-          # interface_read_thread[:sleep_time] = seconds
           interface_read_thread.thread_variable_set(:sleep_time, seconds)
         end
 
         def sleepy?
-          # interface_read_thread[:sleep_enabled] = false
           interface_read_thread.thread_variable_get(:sleep_enabled)
         end
 
         def awake!
           t = interface_read_thread
-          # interface_read_thread[:sleep_enabled] = false
           t.thread_variable_set(:sleep_enabled, false)
-          # puts "[#{t[:name]}] :sleep_enabled => #{sleepy?}"
         end
 
         def sleepy!
           t = interface_read_thread
-          # interface_read_thread[:sleep_enabled] = true
           t.thread_variable_set(:sleep_enabled, true)
-          # puts "[#{t[:name]}] :sleep_enabled => #{sleepy?}"
         end
       end
     end
