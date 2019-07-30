50746e652eef673aa80377674f43d3dcd7a115ba
Piers Holt <piersholt@gmail.com>
2019-07-30 13:22:45 +1000

    wilhelm-virtual: API::Base indentation

--- lib/wilhelm/virtual/device/api/base.rb
+++ lib/wilhelm/virtual/device/api/base.rb
@@ -24,45 +24,51 @@ module Wilhelm
           def try(from, to, command_id, command_arguments = {})
             from = resolve_ident(from)
             to = resolve_ident(to)
-            command_object = to_command(command_id: command_id,
+            command_object = to_command(
+              command_id: command_id,
               command_arguments: command_arguments,
               schema_from: from,
-              schema_to: to)
+              schema_to: to
+            )
 
-              send_it!(from, to, command_object)
-            rescue StandardError => e
-              LOGGER.error("#{self.class} StandardError: #{e}")
-              e.backtrace.each { |l| LOGGER.error(l) }
-              binding.pry
-            end
+            send_it!(from, to, command_object)
+          rescue StandardError => e
+            LOGGER.error("#{self.class} StandardError: #{e}")
+            e.backtrace.each { |l| LOGGER.error(l) }
+            binding.pry
+          end
 
-            def send_it!(from, to, command)
-              LOGGER.debug(name) { "#send_it!(#{from}, #{to}, #{command.inspect})" }
-              message = Message.new(from, to, command)
-              changed
-              notify_observers(MESSAGE_SENT, message: message)
-              true
-            rescue StandardError => e
-              LOGGER.error("#{self.class} StandardError: #{e}")
-              e.backtrace.each { |l| LOGGER.error(l) }
-              binding.pry
-            end
+          def send_it!(from, to, command)
+            LOGGER.debug(name) { "#send_it!(#{from}, #{to}, #{command.inspect})" }
+            message = Message.new(from, to, command)
+            changed
+            notify_observers(MESSAGE_SENT, message: message)
+            true
+          rescue StandardError => e
+            LOGGER.error("#{self.class} StandardError: #{e}")
+            e.backtrace.each { |l| LOGGER.error(l) }
+            binding.pry
+          end
 
-            alias give_it_a_go try
+          alias give_it_a_go try
 
-            private
+          private
 
-            def to_command(command_id:, command_arguments:, schema_from:, schema_to:)
-              command_config = CommandMap.instance.config(command_id, from: schema_from, to: schema_to)
-              command_builder = command_config.builder
-              command_builder = command_builder.new(command_config)
-              command_builder.add_parameters(command_arguments)
-              command_builder.result
-            rescue StandardError => e
-              with_backtrace(LOGGER, e)
-            end
+          def to_command(command_id:, command_arguments:, schema_from:, schema_to:)
+            command_config = CommandMap.instance.config(
+              command_id,
+              from: schema_from,
+              to: schema_to
+            )
+            command_builder = command_config.builder
+            command_builder = command_builder.new(command_config)
+            command_builder.add_parameters(command_arguments)
+            command_builder.result
+          rescue StandardError => e
+            with_backtrace(LOGGER, e)
           end
         end
+      end
     end
   end
 end
