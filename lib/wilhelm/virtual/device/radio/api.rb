bde5e20eb2f8af801fc1373a4d6daa0905e89cd3
Piers Holt <piersholt@gmail.com>
2019-08-01 14:13:24 +1000

    wilhelm-virtual: Radio::API break out Display module

--- lib/wilhelm/virtual/device/radio/api.rb
+++ lib/wilhelm/virtual/device/radio/api.rb
@@ -1,28 +1,17 @@
 # frozen_string_literal: true
 
+require_relative 'api/display'
 require_relative 'api/led'
 
 module Wilhelm
   module Virtual
     class Device
       module Radio
-        # Radio API
+        # Radio::API
         module API
-          include Constants::Command::Aliases
           include Device::API::BaseAPI
+          include Display
           include LED
-
-          # BMBT
-
-          # 0x4A
-          # def led(from: :rad, to: :bmbt, **arguments)
-          #   format_chars!(arguments)
-          #   try(from, to, RAD_LED, arguments)
-          # end
-          #
-          # def l(i)
-          #   led(led: i)
-          # end
 
           # CD CHANGER
 
@@ -42,32 +31,6 @@ module Wilhelm
           def rad_alt(from: :rad, to: :gfx, **arguments)
             try(from, to, RAD_ALT, arguments)
           end
-
-          # DISPLAY
-
-          # 0x23
-          def draw_23(from: :rad, to: :gfx, **arguments)
-            format_chars!(arguments)
-            try(from, to, TXT_GFX, arguments)
-          end
-
-          alias primary draw_23
-
-          # 0xA5
-          def draw_a5(from: :rad, to: :gfx, **arguments)
-            format_chars!(arguments)
-            try(from, to, TXT_NAV, arguments)
-          end
-
-          alias secondary draw_a5
-
-          # 0x21
-          def draw_21(from: :rad, to: :gfx, **arguments)
-            format_chars!(arguments)
-            try(from, to, TXT_MID, arguments)
-          end
-
-          alias list draw_21
         end
       end
     end
---
+++ lib/wilhelm/virtual/device/radio/api/display.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Wilhelm
+  module Virtual
+    class Device
+      module Radio
+        module API
+          # Radio::API::Display
+          module Display
+            include Device::API::BaseAPI
+
+            # 0x23
+            def draw_23(from: :rad, to: :gfx, **arguments)
+              format_chars!(arguments)
+              try(from, to, TXT_GFX, arguments)
+            end
+
+            alias primary draw_23
+
+            # 0x21
+            def draw_21(from: :rad, to: :gfx, **arguments)
+              format_chars!(arguments)
+              try(from, to, TXT_MID, arguments)
+            end
+
+            alias list draw_21
+
+            # 0xA5
+            def draw_a5(from: :rad, to: :gfx, **arguments)
+              format_chars!(arguments)
+              try(from, to, TXT_NAV, arguments)
+            end
+
+            alias secondary draw_a5
+          end
+        end
+      end
+    end
+  end
+end
