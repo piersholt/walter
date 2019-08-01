3728e67917019229d39bbddb080ce507aecdd880
Piers Holt <piersholt@gmail.com>
2019-08-01 14:49:19 +1000

    wilhelm-virtual: Handler::Packet logging constants and style guide updates

--- lib/wilhelm/virtual/handler/packet.rb
+++ lib/wilhelm/virtual/handler/packet.rb
@@ -1,11 +1,14 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
 module Wilhelm
   module Virtual
     module Handler
-      # Comment
+      # Virtual::Handler::PacketHandler
       class PacketHandler < Core::BaseHandler
         include LogActually::ErrorOutput
+
+        PROG = 'Handler::Packet'
+        LOG_BUILD_COMMAND = '#build_command'
 
         def initialize(bus, address_lookup_table = AddressLookupTable.instance)
           @bus = bus
@@ -18,43 +21,44 @@ module Wilhelm
           return false unless addressable?(packet)
           message = parse_packet(packet)
           changed
-          # LOGGER.warn(self.class) { "notify_observers(MESSAGE_RECEIVED, message: message)" }
           notify_observers(MESSAGE_RECEIVED, message: message)
         end
 
         private
 
         def resolve_addresses(packet)
-          LOGGER.debug(name) { "#resolve_addresses(#{packet})" }
+          LOGGER.debug(PROG) { "#resolve_addresses(#{packet})" }
 
           from = @address_lookup_table.resolve_address(packet.from)
           packet.from = from
-          LOGGER.debug(name) { "from_device: #{packet.sender}" }
+          LOGGER.debug(PROG) { "from_device: #{packet.sender}" }
 
           to = @address_lookup_table.resolve_address(packet.to)
           packet.to = to
-          LOGGER.debug(name) { "to_device: #{packet.receiver}" }
+          LOGGER.debug(PROG) { "to_device: #{packet.receiver}" }
 
           packet
         end
 
         def resolve_idents(message)
-          LOGGER.debug(name) { "#resolve_addresses(#{message})" }
+          LOGGER.debug(PROG) { "#resolve_addresses(#{message})" }
 
           from = @address_lookup_table.resolve_ident(message.from)
           message.sender = from
-          LOGGER.debug(name) { "from_device: #{message.sender}" }
+          LOGGER.debug(PROG) { "from_device: #{message.sender}" }
 
           to = @address_lookup_table.resolve_ident(message.to)
           message.receiver = to
-          LOGGER.debug(name) { "to_device: #{message.receiver}" }
+          LOGGER.debug(PROG) { "to_device: #{message.receiver}" }
 
           message
         end
 
+        ERROR_PACKET_NIL = 'Packet is nil!'
+
         def packet?(properties)
           packet = fetch(properties, :packet)
-          raise RoutingError, 'Packet is nil!' unless packet
+          raise(RoutingError, ERROR_PACKET_NIL) unless packet
           packet
         end
 
@@ -68,12 +72,12 @@ module Wilhelm
 
         def bus_has_sender?(from_ident)
           has_from = bus_has_device?(from_ident)
-          raise RoutingError, "#{from_ident} is not on the bus." unless has_from
+          raise(RoutingError, "#{from_ident} is not on bus!") unless has_from
         end
 
         def bus_has_recipient?(to_ident)
           has_to = bus_has_device?(to_ident)
-          raise RoutingError, "#{to_ident} is not on the bus." unless has_to
+          raise(RoutingError, "#{to_ident} is not on bus!") unless has_to
         end
 
         def bus_has_device?(device_ident)
@@ -94,62 +98,53 @@ module Wilhelm
         end
 
         def parse_command(from_ident, to_ident, command, arguments)
-          command_config = CommandMap.instance.config(command, from: from_ident, to: to_ident)
+          command_config =
+            CommandMap.instance.config(command, from: from_ident, to: to_ident)
           parameter_values_hash = parse_argumets(command_config, arguments)
-          LOGGER.debug(self.class) { "Parameter Values: #{parameter_values_hash}" }
+          LOGGER.debug(PROG) { "Parameter Values: #{parameter_values_hash}" }
           command_object = build_command(command_config, parameter_values_hash)
-          # LOGGER.info(ident) { command_object }
           command_object
         end
 
-        private
-
         def parse_argumets(command_config, arguments)
-          # LOGGER.info(ident) { "#parse_argumets" }
           if command_config.has_parameters? && !command_config.is_base?
-            # LOGGER.info(ident) { "#{command_config.sn} has a klass and parameters. Will parse." }
             parse_indexed_arguments(command_config, arguments)
           else
-            # LOGGER.info(ident) { "#{command_config.sn} is getting plain old arguments." }
             arguments
           end
         end
 
         def parse_indexed_arguments(command_config, arguments)
           parameter_values_hash = {}
-          begin
-            argument_index = command_config.index
-            LOGGER.debug(self.class) { "argument index: #{argument_index}" }
-            indexed_arguments = IndexedArguments.new(arguments, argument_index)
-            LOGGER.debug(self.class) { "indexed_arguments: #{indexed_arguments}" }
+          argument_index = command_config.index
+          LOGGER.debug(PROG) { "argument index: #{argument_index}" }
+          indexed_arguments = IndexedArguments.new(arguments, argument_index)
+          LOGGER.debug(PROG) { "indexed_arguments: #{indexed_arguments}" }
 
-            indexed_arguments.parameters.each do |name|
-              param_value = indexed_arguments.lookup(name)
-              LOGGER.debug(self.class) { "indexed_arguments.lookup(#{name}) => #{param_value ? param_value : 'NULL'}" }
-              parameter_values_hash[name] = param_value
-            end
-
-            parameter_values_hash
-          rescue StandardError => e
-            LOGGER.error(self.class) { "#{e}" }
-            e.backtrace.each { |l| LOGGER.error(l) }
-            binding.pry
+          indexed_arguments.parameters.each do |name|
+            param_value = indexed_arguments.lookup(name)
+            LOGGER.debug(PROG) { "indexed_arguments.lookup(#{name}) => #{param_value ? param_value : 'nil'}" }
+            parameter_values_hash[name] = param_value
           end
+
+          parameter_values_hash
+        rescue StandardError => e
+          LOGGER.error(PROG) { e }
+          e.backtrace.each { |line| LOGGER.error(PROG) { line } }
+          binding.pry
         end
 
         def build_command(command_config, parameter_values_hash)
-          LOGGER.debug(self.class) { "#build_command" }
-          begin
-            command_builder = command_config.builder
-            command_builder = command_builder.new(command_config)
-            command_builder.add_parameters(parameter_values_hash)
+          LOGGER.debug(PROG) { LOG_BUILD_COMMAND }
+          command_builder = command_config.builder
+          command_builder = command_builder.new(command_config)
+          command_builder.add_parameters(parameter_values_hash)
 
-            command_builder.result
-          rescue StandardError => e
-            LOGGER.error(self.class) { "#{e}" }
-            e.backtrace.each { |l| LOGGER.error(l) }
-            binding.pry
-          end
+          command_builder.result
+        rescue StandardError => e
+          LOGGER.error(PROG) { e }
+          e.backtrace.each { |line| LOGGER.error(PROG) { line } }
+          binding.pry
         end
       end
     end
