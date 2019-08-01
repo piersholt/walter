1248885ee3fb0c65ae5dab1613dc433a652cbb71
Piers Holt <piersholt@gmail.com>
2019-08-01 16:59:18 +1000

    wilhelm-virtual: rename Handler::Packet source files

--- lib/wilhelm/virtual/handler.rb
+++ lib/wilhelm/virtual/handler.rb
@@ -4,7 +4,7 @@ puts "\tLoading wilhelm/virtual/handler"
 
 require_relative 'handler/error'
 require_relative 'handler/interface'
-require_relative 'handler/packet'
+require_relative 'handler/data'
 require_relative 'handler/message'
 require_relative 'handler/display'
 require_relative 'handler/session'
