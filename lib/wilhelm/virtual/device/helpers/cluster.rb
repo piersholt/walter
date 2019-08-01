4d7413cd880c3e6fa41025a8a16736d87e4e5eb2
Piers Holt <piersholt@gmail.com>
2019-08-02 08:57:05 +1000

    wilhelm-helpers: Cluster remove deprecated methods

--- lib/wilhelm/virtual/device/helpers/cluster.rb
+++ lib/wilhelm/virtual/device/helpers/cluster.rb
@@ -19,14 +19,6 @@ module Wilhelm
             chars_string.public_send(alignment, HUD_SIZE)
           end
 
-          def centered(chars_string, opts = { upcase: true })
-            upcase = opts[:upcase]
-
-            chars_string = chars_string.center(HUD_SIZE)
-            chars_string = chars_string.upcase if upcase
-            chars_string
-          end
-
           def format_chars!(command_arguments, opts = { align: :center })
             chars_string = command_arguments[:chars]
             return false unless chars_string
@@ -35,16 +27,6 @@ module Wilhelm
 
             chars_array = chars_string.bytes
             command_arguments[:chars] = chars_array
-          end
-
-          def encoding
-            (0..255).step(10) do |x|
-              line = 20.times.map do |i|
-                "#{(x+i).chr rescue 0}"
-              end.join(' ')
-              @bus_device.displays({ gfx: 0x40, ike: 0x30, chars: line }, 0xC8, 0x80 )
-              Kernel.sleep(5)
-            end
           end
         end
       end
