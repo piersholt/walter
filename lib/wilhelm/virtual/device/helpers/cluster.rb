15137111ae5caa67dcced6b5a5f62aa10f665e92
Piers Holt <piersholt@gmail.com>
2019-08-02 09:01:19 +1000

    wilhelm-helpers: Cluster refactor #format_chars! for robustness

--- lib/wilhelm/virtual/device/helpers/cluster.rb
+++ lib/wilhelm/virtual/device/helpers/cluster.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
 module Wilhelm
   module Virtual
@@ -20,13 +20,29 @@ module Wilhelm
           end
 
           def format_chars!(command_arguments, opts = { align: :center })
-            chars_string = command_arguments[:chars]
-            return false unless chars_string
+            return false unless command_arguments.key?(:chars)
+            chars_string = command_arguments.fetch(:chars)
+            return false if char_array?(chars_string)
+            validate_chars_string(chars_string)
 
             align(chars_string, opts[:align])
 
-            chars_array = chars_string.bytes
-            command_arguments[:chars] = chars_array
+            command_arguments[:chars] = chars_string.bytes
+          end
+
+          def validate_chars_string(chars_string)
+            return true if chars_string.is_a?(String)
+            raise(ArgumentError, error_chars_type(chars_string))
+          end
+
+          def error_chars_type(chars_string)
+            "format_chars!: chars must be String, but is #{chars_string.class}"
+          end
+
+          def chars_array?(chars_string)
+            return false unless chars_string.is_a?(Array)
+            return false unless chars_string.all? { |i| i.is_a?(Integer) }
+            true
           end
         end
       end
