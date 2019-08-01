816977a0a5204aae1caa70be3121fbbfc3360f23
Piers Holt <piersholt@gmail.com>
2019-08-01 14:26:40 +1000

    wilhelm-api: Telephone simplify LED use

--- lib/wilhelm/api/telephone/led.rb
+++ lib/wilhelm/api/telephone/led.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
 module Wilhelm
   module API
@@ -13,37 +13,45 @@ module Wilhelm
         #   connect/disconnect modifier
         #   contextualises connection operation as disconnection
 
+        MOD_PROG = 'Telephone::LED'
+
         # green
         def connected
+          logger.debug(MOD_PROG) { '#connected' }
           bus.tel.red(:off).green(:on).leds!
         end
 
         def disconnected
+          logger.debug(MOD_PROG) { '#disconnected' }
           bus.tel.red(:on).green(:off).leds!
         end
 
         # yellow
         # attempt to begin connection operation
         def connect
+          logger.debug(MOD_PROG) { '#connect' }
+          bus.tel.green(:blink).leds!
+        end
+
+        # yellow blink
+        # acknowledged operation via bluetooth service callback
+        def connecting
+          logger.debug(MOD_PROG) { '#connecting' }
           bus.tel.green(:blink).leds!
         end
 
         # red
         # attempt to begin disconnection operation
         def disconnect
+          logger.debug(MOD_PROG) { '#disconnect' }
           bus.tel.red(:blink).leds!
-        end
-
-        # yellow blink
-        # acknowledged operation via bluetooth service callback
-        def connecting
-          bus.tel.green(:blink).leds!
         end
 
         # red blink
         # acknowledged operation via bluetooth service callback
         def disconnecting
-          bus.tel.red(:blink).leds!
+          logger.debug(MOD_PROG) { '#disconnecting' }
+          bus.tel.red(:blink).green(:off).leds!
         end
       end
     end
