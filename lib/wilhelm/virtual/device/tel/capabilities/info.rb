4c61047f9e9743589b5bc703694d9b3c9611c571
Piers Holt <piersholt@gmail.com>
2019-08-02 21:03:03 +1000

    wilhelm-virtual: update 0x24 parameter names

--- lib/wilhelm/virtual/device/tel/capabilities/info.rb
+++ lib/wilhelm/virtual/device/tel/capabilities/info.rb
@@ -25,36 +25,36 @@ module Wilhelm
 
             # 0x91 Strength
             def strength(level = Random.rand(0..7))
-              anzv_var_tel(gfx: STRENGTH, chars: bars(level))
+              anzv_var_tel(field: STRENGTH, chars: bars(level))
             end
 
             # 0x91 Strength
             def strength!(chars)
-              anzv_var_tel(gfx: STRENGTH, chars: chars)
+              anzv_var_tel(field: STRENGTH, chars: chars)
             end
 
             # 0x93: Call cost current
             # "     0"
             def call_costs_current(current_cost = Random.rand(0..9).to_s)
-              anzv_var_tel(gfx: CALL_COST_CURRENT, chars: current_cost)
+              anzv_var_tel(field: CALL_COST_CURRENT, chars: current_cost)
             end
 
             # 0x94: Call cost total
             # "      0"
             def call_cost_total(total_cost = Random.rand(10..99).to_s)
-              anzv_var_tel(gfx: CALL_COST_TOTAL, chars: total_cost)
+              anzv_var_tel(field: CALL_COST_TOTAL, chars: total_cost)
             end
 
             # 0x96: Call Time minutes
             # "  0" {1,3}
             def call_time_minutes(minutes = Random.rand(0..59).to_s)
-              anzv_var_tel(gfx: CALL_TIME_MINUTES, chars: minutes)
+              anzv_var_tel(field: CALL_TIME_MINUTES, chars: minutes)
             end
 
             # 0x97: Call time seconds
             # " 0" {2,2}
             def call_time_seconds(seconds = Random.rand(0..59).to_s)
-              anzv_var_tel(gfx: CALL_TIME_SECONDS, chars: seconds)
+              anzv_var_tel(field: CALL_TIME_SECONDS, chars: seconds)
             end
 
             private
