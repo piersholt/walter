f99f8febdeee3e4229e6799c18f34659d227bf52
Piers Holt <piersholt@gmail.com>
2019-08-03 16:25:59 +1000

    wilhelm-virtual: MappedParameter refactor

--- lib/wilhelm/virtual/protocol/commands/paramaterized/parameter/mapped_parameter.rb
+++ lib/wilhelm/virtual/protocol/commands/paramaterized/parameter/mapped_parameter.rb
@@ -2,8 +2,18 @@
 
 module Wilhelm
   module Virtual
+    # Virtual::MappedParameter
     class MappedParameter < BaseParameter
       PROC = 'MappedParameter'.freeze
+
+      PRETTY_VALUE_NIL = '--'.freeze
+      DICTIONARY_NIL   = '@dictionary is nil!'.freeze
+      MAP_NIL          = '@map is nil!'.freeze
+      WARN_DICT_NIL    = 'Map @dictionary is nil!'.freeze
+      WARN_MAP_NIL     = 'Map @map is nil!'.freeze
+      ERROR_VALUE_NIL  = '#ugly: value is nil!'.freeze
+
+      MASK_LABEL = "%-#{DEFAULT_LABEL_WIDTH}s".freeze
 
       attr_accessor :map, :dictionary, :label
       def initialize(configuration, integer)
@@ -14,21 +24,20 @@ module Wilhelm
         "<#{PROC} @value=#{value}>"
       end
 
-      def to_s(width = DEFAULT_LABEL_WIDTH)
+      def to_s
         str_buffer = ''
-        str_buffer.concat(format("%-#{width}s", "#{label}")) if label
+        str_buffer.concat(format(MASK_LABEL, label)) if label
         str_buffer.concat(LABEL_DELIMITER) if label
         str_buffer.concat(pretty.to_s)
       end
 
       def ugly
-        if value.nil?
-          raise ArgumentError, 'No map to get ugly :map'
-        elsif @map.nil?
-          LOGGER.warn(PROC) { "Map @map is nil!" }
-          return "\"value\""
+        raise(ArgumentError, ERROR_VALUE_NIL) unless value
+        if @map.nil?
+          LOGGER.warn(PROC) { WARN_MAP_NIL }
+          DICTIONARY_NIL
         elsif !@map.key?(value)
-          return "#{d2h(value, true)} not found!"
+          value_not_found(value)
         else
           @map[value]
         end
@@ -37,16 +46,19 @@ module Wilhelm
       alias to_sym ugly
 
       def pretty
-        if value.nil?
-          return '--'
-        elsif @dictionary.nil?
-          LOGGER.warn(PROC) { "Map @dictionary is nil!" }
-          return "\"value\""
+        return PRETTY_VALUE_NIL unless value
+        if @dictionary.nil?
+          LOGGER.warn(PROC) { WARN_DICT_NIL }
+          MAP_NIL
         elsif !@dictionary.key?(value)
-          return "#{d2h(value, true)} not found!"
+          value_not_found(value)
         else
           @dictionary[value]
         end
+      end
+
+      def value_not_found(value)
+        "#{d2h(value, true)} not found!"
       end
     end
   end
