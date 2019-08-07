84acc6bc9bb8777ffe5f7c5d341cc1626b1cda9e
Piers Holt <piersholt@gmail.com>
2019-08-07 16:56:33 +1000

    wilhelm-virtual: clean up Command load paths

--- lib/wilhelm/virtual/protocol/command.rb
+++ lib/wilhelm/virtual/protocol/command.rb
@@ -1,32 +1,22 @@
 # frozen_string_literal: true
 
-# @deprecated ?
+puts "\tLoading wilhelm/virtual/protocol/command"
+
 require_relative 'command/chars'
 
-puts "\tLoading wilhelm/virtual/protocol/commands"
 require_relative 'command/base'
+
 require_relative 'command/ccm_display'
-require_relative 'command/media_display'
-require_relative 'command/temperature'
-require_relative 'command/speed'
-require_relative 'command/key'
 require_relative 'command/ike_sensors'
+require_relative 'command/key'
 require_relative 'command/lamp'
+require_relative 'command/media_display'
+require_relative 'command/mid'
 require_relative 'command/radio_led'
+require_relative 'command/speed'
+require_relative 'command/temperature'
 
-puts "\tLoading wilhelm/virtual/protocol/command/configuration"
 require_relative 'command/configuration'
-
-puts "\tLoading wilhelm/virtual/protocol/command/builder"
 require_relative 'command/builder'
-
-puts "\tLoading wilhelm/virtual/protocol/command/paramaterized/model"
-# require_relative 'command/paramaterized/model/indexed'
-# require_relative 'command/paramaterized/model/bit_array'
-# require_relative 'command/paramaterized/model/indexed_arguments'
-
-puts "\tLoading wilhelm/virtual/protocol/command/paramaterized"
 require_relative 'command/paramaterized'
-
-puts "\tLoading wilhelm/virtual/protocol/command/paramaterized/parameter"
 require_relative 'command/paramaterized/parameter'
