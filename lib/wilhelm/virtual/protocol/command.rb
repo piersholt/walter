1648a68fa9dcefbf4f54771fc12c07bcab7be617
Piers Holt <piersholt@gmail.com>
2019-08-08 13:32:25 +1000

    wilhelm-virtual: Command remove Base subclasses

--- lib/wilhelm/virtual/protocol/command.rb
+++ lib/wilhelm/virtual/protocol/command.rb
@@ -6,16 +6,7 @@ require_relative 'command/chars'
 
 require_relative 'command/base'
 
-require_relative 'command/ccm_display'
-require_relative 'command/ike_sensors'
-require_relative 'command/key'
-require_relative 'command/lamp'
-require_relative 'command/media_display'
-require_relative 'command/mid'
 require_relative 'command/radio_led'
-require_relative 'command/speed'
-require_relative 'command/temperature'
-
 require_relative 'command/configuration'
 require_relative 'command/builder'
 require_relative 'command/paramaterized'
