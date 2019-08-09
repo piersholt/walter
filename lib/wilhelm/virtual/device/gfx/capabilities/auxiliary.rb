3b46bf93260dd4f8265f1be0072ec65ad6d8c930
Piers Holt <piersholt@gmail.com>
2019-08-09 12:30:18 +1000

    wilhelm-virtual: GFX::Capabilities::Auxiliary updates

--- lib/wilhelm/virtual/device/gfx/capabilities/auxiliary.rb
+++ lib/wilhelm/virtual/device/gfx/capabilities/auxiliary.rb
@@ -10,6 +10,8 @@ module Wilhelm
             include API
             include Constants
 
+            # TIMER 1 ---------------------------------------------------------
+
             # Auxiliary Timer 1 0x0f [On/Off]
             # @note alias at1
             def auxiliary_timer_1(enabled = true)
@@ -18,6 +20,17 @@ module Wilhelm
                 control: enabled ? CONTROL_ON : CONTROL_OFF
               )
             end
+
+            alias at1 auxiliary_timer_1
+
+            # Auxiliary Timer 1 0x0F
+            def input_auxiliary_timer_1(hour = hour?, minute = min?)
+              return false unless valid_time?(hour, minute)
+              hour = to_twelve_hour_time(hour)
+              obc_var(field: FIELD_AUX_TIMER_1, input: [hour, minute])
+            end
+
+            # TIMER 2 ---------------------------------------------------------
 
             # Auxiliary Timer 2 0x10 [On/Off]
             # @note alias at2
@@ -28,8 +41,20 @@ module Wilhelm
               )
             end
 
-            alias at1 auxiliary_timer_1
+            # Auxiliary Timer 2 0x10
+            def input_auxiliary_timer_2(hour = hour?, minute = min?)
+              return false unless valid_time?(hour, minute)
+              hour = to_twelve_hour_time(hour)
+              obc_var(field: FIELD_AUX_TIMER_2, input: [hour, minute])
+            end
+
             alias at2 auxiliary_timer_2
+
+            # DIRECT ----------------------------------------------------------
+
+            def auxiliary_heating(*); end
+
+            def auxiliary_ventilation(*); end
           end
         end
       end
--- lib/wilhelm/virtual/device/ike/capabilities/auxiliary.rb
+++ lib/wilhelm/virtual/device/ike/capabilities/auxiliary.rb
@@ -10,21 +10,6 @@ module Wilhelm
             include API
             include Helpers::Data
 
-            # Auxiliary Timer 1
-            # @note alias at1
-            def auxiliary_timer_1(enabled = true)
-
-            end
-
-            # Auxiliary Timer 2
-            # @note alias at2
-            def auxiliary_timer_2(enabled = true)
-
-            end
-
-            alias at1 auxiliary_timer_1
-            alias at2 auxiliary_timer_2
-
             def auxiliary_direct(enabled = true)
               anzv_bool(control_a: x, control_b: y)
             end
