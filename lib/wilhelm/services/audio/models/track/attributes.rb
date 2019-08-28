d733ec58aa69ce80b00864a3c97376976bf8c14c
Piers Holt <piersholt@gmail.com>
2019-08-28 16:38:58 +1000

    wilhelm-services: Audio::Track::Attributes

---
+++ lib/wilhelm/services/audio/models/track/attributes.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module Wilhelm
+  module Services
+    class Audio
+      class Track
+        # Audio::Track::Attributes
+        module Attributes
+          include Constants
+
+          attr_reader :attributes
+
+          # Track
+
+          def title
+            attributes.fetch(TITLE, EMPTY_ATTRIBUTE)
+          end
+
+          def artist
+            attributes.fetch(ARTIST, EMPTY_ATTRIBUTE)
+          end
+
+          def album
+            attributes.fetch(ALBUM, EMPTY_ATTRIBUTE)
+          end
+
+          def track_number
+            attributes.fetch(TRACK_NUMBER, EMPTY_ATTRIBUTE)
+          end
+
+          alias number track_number
+
+          def number_of_tracks
+            attributes.fetch(NUMBER_OF_TRACKS, EMPTY_ATTRIBUTE)
+          end
+
+          alias tracks number_of_tracks
+          alias total number_of_tracks
+
+          def genre
+            attributes.fetch(GENRE, EMPTY_ATTRIBUTE)
+          end
+
+          def duration
+            attributes.fetch(DURATION, EMPTY_ATTRIBUTE)
+          end
+
+          def attributes!(track_attributes)
+            attributes.merge!(track_attributes)
+          end
+        end
+      end
+    end
+  end
+end
