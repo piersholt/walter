272395fcc81a9e9c95b305a2ff32b54578513c34
Piers Holt <piersholt@gmail.com>
2019-08-30 18:08:02 +1000

    wilhelm-api: Display::Handler::Cache style guide updates

--- lib/wilhelm/api/display/handlers/cache_handler.rb
+++ lib/wilhelm/api/display/handlers/cache_handler.rb
@@ -7,29 +7,27 @@ module Wilhelm
       module CacheHandler
         NAME = 'CacheHandler'
 
+        # @note #write! and #cache! are the same method
+
         def header_cache(layout:, index:, chars:)
-          logger.debug(NAME) { "#update(:header_cache, #{layout}, #{index}, #{chars})" }
-          cache.public_send(layout)
-          .cache!(index => chars)
+          logger.debug(NAME) { "#header_cache(#{layout}, #{index}, #{chars})" }
+          cache.public_send(layout).cache!(index => chars)
         end
 
         def header_write(layout:, index:, chars:)
-          logger.debug(NAME) { "#update(:header_write, #{layout}, #{index}, #{chars})" }
-          cache.public_send(layout)
-          .write!(index => chars)
+          logger.debug(NAME) { "#header_write(#{layout}, #{index}, #{chars})" }
+          cache.public_send(layout).write!(index => chars)
         end
 
         def menu_cache(layout:, index:, chars:)
-          logger.debug(NAME) { "#update(:header_cache, #{layout}, #{index}, #{chars})" }
-          cache.public_send(layout)
-          .cache!(index => chars)
+          logger.debug(NAME) { "#menu_cache(#{layout}, #{index}, #{chars})" }
+          cache.public_send(layout).cache!(index => chars)
         end
 
         # This will never have text, it's purely to render.
         def menu_write(layout:, index:, chars:)
-          logger.debug(NAME) { "#update(:menu_write, #{layout}, #{index}, #{chars})" }
-          cache.public_send(layout)
-          .show!
+          logger.debug(NAME) { "#menu_write(#{layout}, #{index}, #{chars})" }
+          cache.public_send(layout).show!
         end
       end
     end
