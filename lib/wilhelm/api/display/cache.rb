349e1acffefd45bcccf0c5a006703389176e48d2
Piers Holt <piersholt@gmail.com>
2019-09-02 15:17:45 +1000

    wilhelm-api: Display::Cache:Base indentation fix

--- lib/wilhelm/api/display/cache.rb
+++ lib/wilhelm/api/display/cache.rb
@@ -17,40 +17,46 @@ module Wilhelm
         end
 
         def menu
-          @menu ||= { basic: Menu::Basic.new,
+          @menu ||= {
+            basic: Menu::Basic.new,
             titled: Menu::Titled.new,
-            static: Menu::Static.new }
-          end
-
-          def digital
-            header[:digital]
-          end
-
-          def basic
-            menu[:basic]
-          end
-
-          def titled
-            menu[:titled]
-          end
-
-          def static
-            menu[:static]
-          end
-
-          def by_layout_id(id)
-            case id
-            when 0x60
-              basic
-            when 0x61
-              titled
-            when 0x63
-              static
-            end
-          end
-
-          alias layout by_layout_id
+            static: Menu::Static.new
+          }
         end
+
+        # 0x62
+        def digital
+          header[:digital]
+        end
+
+        # 0x60
+        def basic
+          menu[:basic]
+        end
+
+        # 0x61
+        def titled
+          menu[:titled]
+        end
+
+        # 0x63
+        def static
+          menu[:static]
+        end
+
+        def by_layout_id(id)
+          case id
+          when 0x60
+            basic
+          when 0x61
+            titled
+          when 0x63
+            static
+          end
+        end
+
+        alias layout by_layout_id
       end
     end
+  end
 end
