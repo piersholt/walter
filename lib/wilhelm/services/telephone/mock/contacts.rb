d45158fe68370b42d2252bdb25ebf3671d616843
Piers Holt <piersholt@gmail.com>
2019-09-19 16:49:16 +1000

    wilhelm-services: WIP Telephone

--- lib/wilhelm/services/telephone/mock/contacts.rb
+++ lib/wilhelm/services/telephone/mock/contacts.rb
@@ -1,46 +1,41 @@
 # frozen_string_literal: false
 
 module Wilhelm
-  module Virtual
-    class Device
-      module Telephone
-        module Capabilities
-          module Mock
-            # Contact Generators
-            module Contacts
-              TOP_8_PAGE_SIZE = 8
+  module Services
+    # Bluetooth Telephone
+    class Telephone
+      # Contact Generators
+      module Contacts
+        TOP_8_PAGE_SIZE = 8
 
-              CONTACTS = [
-                'A.Anteater',
-                'B.Bonkers',
-                'C.Cockatoo',
-                'D.Donkey',
-                'E.Eagle',
-                'F.Fatty',
-                'G.Gonzo',
-                'H.Henderson',
-                'I.India',
-                'J.Jamie',
-                'K.Karen',
-                'L.Lorenzo',
-                'M.Mario',
-                'N.Nelly',
-                'O.Ophelia',
-                'P.Porridge',
-                'Q.Queeny',
-                'R.Rogerson',
-                'S.Smythson'
-              ].freeze
+        CONTACTS = [
+          'A.Anteater',
+          'B.Bonkers',
+          'C.Cockatoo',
+          'D.Donkey',
+          'E.Eagle',
+          'F.Fatty',
+          'G.Gonzo',
+          'H.Henderson',
+          'I.India',
+          'J.Jamie',
+          'K.Karen',
+          'L.Lorenzo',
+          'M.Mario',
+          'N.Nelly',
+          'O.Ophelia',
+          'P.Porridge',
+          'Q.Queeny',
+          'R.Rogerson',
+          'S.Smythson'
+        ].freeze
 
-              def favourites
-                @favourites ||= CONTACTS.dup.reverse!.first(TOP_8_PAGE_SIZE)
-              end
+        def favourites
+          @favourites ||= CONTACTS.dup.reverse!.first(TOP_8_PAGE_SIZE)
+        end
 
-              def phone_book
-                @phone_book ||= CONTACTS.dup
-              end
-            end
-          end
+        def phone_book
+          @phone_book ||= CONTACTS.dup
         end
       end
     end
--- lib/wilhelm/services/telephone/service.rb
+++ lib/wilhelm/services/telephone/service.rb
@@ -6,8 +6,43 @@ module Wilhelm
     class Telephone
       include Helpers::Observation
       include Logging
+      include Contacts
 
-      def initialize(*); end
+      PROG = 'Service::Telephone'
+
+      def initialize(api_object = Wilhelm::API::Telephone.instance)
+        @api_object = api_object
+        @api_object.register_telephone_listener(self)
+      end
+
+      def telephone_update(event, args)
+        logger.unknown(PROG) { "#telephone_update(#{event}, #{args})" }
+        case event
+        when :directory_open
+          generate_directory(args)
+        when :directory_back
+          generate_directory(args)
+        when :directory_forward
+          generate_directory(args)
+        when :directory_select
+          directory_select(args)
+        end
+      end
+
+      private
+
+      def generate_directory(page:, page_size:)
+        LOGGER.unknown(PROG) { "#generate_directory(#{page}, #{page_size})" }
+        contacts = phone_book.rotate(page * page_size)&.first(page_size)
+        @api_object.directory_contact_list(*contacts)
+      end
+
+      def directory_select(index:, page:, page_size:)
+        LOGGER.unknown(PROG) { "#directory_select(#{index}, #{page}, #{page_size})" }
+        contact_name = phone_book.rotate(page * page_size)&.slice(index)
+        LOGGER.unknown(PROG) { "contact_name = #{contact_name}" }
+        @api_object.directory_name(contact_name)
+      end
     end
   end
 end
