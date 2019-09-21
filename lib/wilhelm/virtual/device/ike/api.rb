e1c12c885e61cbf39cdbaa01686f1abe4d6c2ff3
Piers Holt <piersholt@gmail.com>
2019-09-21 15:34:32 +1000

    wilhelm-virtual: Device::IKE: Region, Sensors, Speed, Temperature APIs

--- lib/wilhelm/virtual/device/ike/api.rb
+++ lib/wilhelm/virtual/device/ike/api.rb
@@ -3,7 +3,11 @@
 require_relative 'api/check_control'
 require_relative 'api/ignition'
 require_relative 'api/prog'
+require_relative 'api/region'
 require_relative 'api/settings'
+require_relative 'api/sensors'
+require_relative 'api/temperature'
+require_relative 'api/speed'
 
 module Wilhelm
   module Virtual
@@ -14,7 +18,11 @@ module Wilhelm
           include CheckControl
           include Ignition
           include Prog
+          include Region
+          include Sensors
           include Settings
+          include Speed
+          include Temperature
         end
       end
     end
---
+++ lib/wilhelm/virtual/device/ike/api/sensors.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Wilhelm
+  module Virtual
+    class Device
+      module IKE
+        module API
+          # Device::IKE::API::Sensors
+          module Sensors
+            include Device::API::BaseAPI
+
+            # 0x13 SEN
+            def sensors(from: :ike, to: :glo_l, **arguments)
+              dispatch_raw_command(from, to, SENSORS_REP, arguments)
+            end
+
+            def sensors!(s1, s2, s3 = 0x00)
+              sensors(s1: s1, s2: s2, s3: s3)
+            end
+          end
+        end
+      end
+    end
+  end
+end
