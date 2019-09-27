805acaf5849e428e9266c2b583c6b97ca015c53d
Piers Holt <piersholt@gmail.com>
2019-09-27 22:34:26 +1000

    wilhelm-sdk: WIP: shutdown

--- lib/wilhelm/sdk/context/ui/view/debug/index.rb
+++ lib/wilhelm/sdk/context/ui/view/debug/index.rb
@@ -29,7 +29,7 @@ module Wilhelm
               private
 
               def indexed_options
-                options = %w[Services Encoding]
+                options = %w[Services Encoding Shutdown]
                 options.first(COLUMN_ONE_MAX).map.with_index do |option, index|
                   indexed_option =
                   BaseMenuItem.new(label: option, action: option.downcase.to_sym)
