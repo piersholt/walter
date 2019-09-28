ab135a737779ae8b3a9719b8b6146f4197ce882d
Piers Holt <piersholt@gmail.com>
2019-09-28 10:44:14 +1000

    wilhelm-api: Telephone::Emulated::State::Layout

--- lib/wilhelm/virtual/device/tel/emulated/state.rb
+++ lib/wilhelm/virtual/device/tel/emulated/state.rb
@@ -2,6 +2,7 @@
 
 require_relative 'state/constants'
 require_relative 'state/model'
+require_relative 'state/layout'
 require_relative 'state/status'
 require_relative 'state/led'
 
@@ -15,62 +16,7 @@ module Wilhelm
             include Model
             include Status
             include LED
-
-            def dial!
-              @layout = :dial
-            end
-
-            def dial?
-              @layout == :dial
-            end
-
-            def pin!
-              @layout = :pin
-            end
-
-            def pin?
-              @layout == :pin
-            end
-
-            def directory!
-              @layout = :directory
-            end
-
-            def directory?
-              @layout == :directory
-            end
-
-            def top_8!
-              @layout = :top_8
-            end
-
-            def top_8?
-              @layout == :top_8
-            end
-
-            def info!
-              @layout = :info
-            end
-
-            def info?
-              @layout == :info
-            end
-
-            def sos!
-              @layout = :sos
-            end
-
-            def sos?
-              @layout == :sos
-            end
-
-            def smses!
-              @layout = :smses
-            end
-
-            def smses?
-              @layout == :smses
-            end
+            include Layout
           end
         end
       end
---
+++ lib/wilhelm/virtual/device/tel/emulated/state/layout.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+module Wilhelm
+  module Virtual
+    class Device
+      module Telephone
+        class Emulated < Device::Emulated
+          module State
+            # Telephone::Emulated::State::Layout
+            module Layout
+              # 0x05
+              def pin!
+                @layout = :pin
+              end
+
+              def pin?
+                @layout == :pin
+              end
+
+              # 0x20
+              def info!
+                @layout = :info
+              end
+
+              def info?
+                @layout == :info
+              end
+
+              # 0x42
+              def dial!
+                @layout = :dial
+              end
+
+              def dial?
+                @layout == :dial
+              end
+
+              # 0x43
+              def directory!
+                @layout = :directory
+              end
+
+              def directory?
+                @layout == :directory
+              end
+
+              # 0x80
+              def top_8!
+                @layout = :top_8
+              end
+
+              def top_8?
+                @layout == :top_8
+              end
+
+              # 0xf0
+              def smses!
+                @layout = :smses
+              end
+
+              def smses?
+                @layout == :smses
+              end
+
+              # 0xf1
+              def sms!
+                @layout = :sms
+              end
+
+              def sms?
+                @layout == :sms
+              end
+
+              # 0xf1
+              def sos!
+                @layout = :sos
+              end
+
+              def sos?
+                @layout == :sos
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
