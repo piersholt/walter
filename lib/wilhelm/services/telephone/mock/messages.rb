9a1bb7944f2d3b26994463bef6413538df67baae
Piers Holt <piersholt@gmail.com>
2019-10-02 13:05:10 +1000

    wilhelm-services: Telephone::Mock::Messages frozen string error

--- lib/wilhelm/services/telephone/mock/messages.rb
+++ lib/wilhelm/services/telephone/mock/messages.rb
@@ -15,25 +15,25 @@ module Wilhelm
             .freeze
 
           MESSAGES = [
-            Message.new('A. Message', BODY),
-            Message.new('B. Message', BODY),
-            Message.new('C. Message', BODY),
-            Message.new('D. Message', BODY),
-            Message.new('E. Message', BODY),
-            Message.new('F. Message', BODY),
-            Message.new('G. Message', BODY),
-            Message.new('H. Message', BODY),
-            Message.new('I. Message', BODY),
-            Message.new('J. Message', BODY),
-            Message.new('K. Message', BODY),
-            Message.new('L. Message', BODY),
-            Message.new('M. Message', BODY),
-            Message.new('N. Message', BODY),
-            Message.new('O. Message', BODY),
-            Message.new('P. Message', BODY),
-            Message.new('Q. Message', BODY),
-            Message.new('R. Message', BODY),
-            Message.new('S. Message', BODY)
+            Message.new('A. Message', BODY.dup),
+            Message.new('B. Message', BODY.dup),
+            Message.new('C. Message', BODY.dup),
+            Message.new('D. Message', BODY.dup),
+            Message.new('E. Message', BODY.dup),
+            Message.new('F. Message', BODY.dup),
+            Message.new('G. Message', BODY.dup),
+            Message.new('H. Message', BODY.dup),
+            Message.new('I. Message', BODY.dup),
+            Message.new('J. Message', BODY.dup),
+            Message.new('K. Message', BODY.dup),
+            Message.new('L. Message', BODY.dup),
+            Message.new('M. Message', BODY.dup),
+            Message.new('N. Message', BODY.dup),
+            Message.new('O. Message', BODY.dup),
+            Message.new('P. Message', BODY.dup),
+            Message.new('Q. Message', BODY.dup),
+            Message.new('R. Message', BODY.dup),
+            Message.new('S. Message', BODY.dup)
           ].freeze
 
           def messages
