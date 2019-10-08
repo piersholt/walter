97560b3fe4e26e2f676e0e90615906a391a0af93
Piers Holt <piersholt@gmail.com>
2019-10-08 16:01:08 +1100

    wilhelm-helpers: SignedMagnitude#generate

--- lib/wilhelm/helpers/signed_magnitude.rb
+++ lib/wilhelm/helpers/signed_magnitude.rb
@@ -6,33 +6,51 @@ module Wilhelm
     # BMBT LCD brightness control has values -10 through 10 as indicated
     # via Service Mode. These values appear to be SMR.
     class SignedMagnitude
-      extend Forwardable
       include Wilhelm::Helpers::Bitwise
 
       BITMASK_SIGN      = 0b1000_0000
       BITMASK_MAGNITUDE = 0b0111_1111
 
       attr_reader :value
-      def_delegators :value, *Integer.instance_methods(false)
 
-      def initialize(int)
-        validate_value(int)
-
-        sign = bitwise_all?(int, BITMASK_SIGN)
-        magnitude = bitwise_and(int, BITMASK_MAGNITUDE)
+      def parse(smr)
+        validate_smr(smr)
+        sign = bitwise_all?(smr, BITMASK_SIGN)
+        magnitude = bitwise_and(smr, BITMASK_MAGNITUDE)
         @value = magnitude
         @value *= -1 if sign
+        self
       end
 
-      def self.parse(int)
-        smr = new(int)
-        smr.value
+      def generate(int)
+        validate_int(int)
+        @value = 0
+        @value = bitwise_xor(@value, BITMASK_SIGN) unless int.positive?
+        @value = bitwise_xor(@value, int.abs)
+        self
+      end
+
+      # @param SignedMagnitude smr
+      # @return Integer
+      def self.parse(smr)
+        new.parse(smr)&.value
+      end
+
+      # @param Integer int
+      # @return SignedMagnitude
+      def self.generate(int)
+        new.generate(int)&.value
       end
 
       private
 
-      def validate_value(int)
-        return true if (0x00..0xff).cover?(int)
+      def validate_smr(smr)
+        return true if (0x00..0xff).cover?(smr)
+        raise(ArgumentError, "Range error: #{smr}")
+      end
+
+      def validate_int(int)
+        return true if (-127..127).cover?(int)
         raise(ArgumentError, "Range error: #{int}")
       end
     end
