2da17fe403628948bb6aa45ad66deffa233981a9
Piers Holt <piersholt@gmail.com>
2019-10-11 13:13:51 +1100

    wilhelm-virtual: API::Telephone remove nested Telephone load path

--- lib/wilhelm/api/telephone.rb
+++ lib/wilhelm/api/telephone.rb
@@ -12,4 +12,39 @@ require_relative 'telephone/sos'
 require_relative 'telephone/status'
 
 require_relative 'telephone/listener'
-require_relative 'telephone/telephone'
+
+module Wilhelm
+  module API
+    # Telephone object for vehicle API
+    class Telephone
+      include Singleton
+      include Helpers::Observation
+
+      include Listener
+      include Directory
+      include Info
+      include LastNumbers
+      include Top8
+      include LED
+      include SMS
+      include Status
+      include SOS
+
+      PROG = 'Telephone'
+
+      attr_accessor :bus
+
+      def logger
+        LOGGER
+      end
+
+      def to_s
+        PROG
+      end
+
+      def targets
+        %i[tel]
+      end
+    end
+  end
+end
