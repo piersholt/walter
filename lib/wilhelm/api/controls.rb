e8392c97900ffbf12695019d2fef32c5f009e9fa
Piers Holt <piersholt@gmail.com>
2019-10-19 10:40:38 +1100

    wilhelm-api: load path convention

--- lib/wilhelm/api/controls.rb
+++ lib/wilhelm/api/controls.rb
@@ -9,4 +9,44 @@ require_relative 'controls/control/stateful'
 require_relative 'controls/control/two_stage'
 
 require_relative 'controls/control'
-require_relative 'controls/controls'
+
+# TODO
+# - EQ Hifi
+# - EQ Top HiFi (DSP)
+
+module Wilhelm
+  module API
+    # Controls object for vehicle API
+    class Controls
+      STATELESS = :stateless
+      STATEFUL = :stateful
+
+      include Singleton
+      include Observable
+      include Wilhelm::Helpers::Stateful
+      include Listener
+
+      attr_accessor :bus
+
+      alias control_state state
+
+      # HELPERS ----------------------------------------------------
+
+      def logger
+        LOGGER
+      end
+
+      def to_s
+        'Controls'
+      end
+
+      def targets
+        %i[mfl bmbt rad tel]
+      end
+
+      def default_state
+        { mfl: {}, bmbt: {} }
+      end
+    end
+  end
+end
