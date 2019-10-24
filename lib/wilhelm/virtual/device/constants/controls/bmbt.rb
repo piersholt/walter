52968e5282a91e651ae53cbba46ec9e28a25d013
Piers Holt <piersholt@gmail.com>
2019-10-24 18:20:55 +1100

    wilhelm-virtual: Constants::Controls refactor

--- lib/wilhelm/virtual/device/constants/controls/bmbt.rb
+++ lib/wilhelm/virtual/device/constants/controls/bmbt.rb
@@ -6,31 +6,33 @@ module Wilhelm
       module Controls
         # Controls::BMBT
         module BMBT
-          # Right Panel
-          BMBT_TEL = :bmbt_tel
-          BMBT_AUX_HEAT = :bmbt_aux_heat
-          BMBT_NEXT = :bmbt_next
-          BMBT_PREV = :bmbt_prev
-          BMBT_MENU = :bmbt_menu
-
-          # Rotary Navigation
-          BMBT_LEFT = :bmbt_left
-          BMBT_RIGHT = :bmbt_right
-          BMBT_CONFIRM = :bmbt_confirm
-
           # Left Panel
-          BMBT_MODE_PREV = :bmbt_mode_prev
-          BMBT_MODE_NEXT = :bmbt_mode_next
-          BMBT_OVERLAY = :bmbt_overlay
+          BMBT_MODE_PREV    = :bmbt_mode_prev
+          BMBT_MODE_NEXT    = :bmbt_mode_next
+          BMBT_OVERLAY      = :bmbt_overlay
 
           # Rotary Radio
           BMBT_VOL_DOWN     = :vol_down
-          BMBT_VOL_UP       = :vol_up
           BMBT_VOL_DOWN_RAD = :vol_rad_down
-          BMBT_VOL_UP_RAD   = :vol_rad_up
           BMBT_VOL_DOWN_TEL = :vol_tel_down
+
+          BMBT_VOL_UP       = :vol_up
+          BMBT_VOL_UP_RAD   = :vol_rad_up
           BMBT_VOL_UP_TEL   = :vol_tel_up
-          BMBT_POWER = :bmbt_power
+
+          BMBT_POWER        = :bmbt_power
+
+          # Right Panel
+          BMBT_TEL          = :bmbt_tel
+          BMBT_AUX_HEAT     = :bmbt_aux_heat
+          BMBT_NEXT         = :bmbt_next
+          BMBT_PREV         = :bmbt_prev
+          BMBT_MENU         = :bmbt_menu
+
+          # Rotary Navigation
+          BMBT_LEFT         = :bmbt_left
+          BMBT_RIGHT        = :bmbt_right
+          BMBT_CONFIRM      = :bmbt_confirm
 
           BMBT_CONSTANTS = constants.map { |c| const_get(c) }
 
