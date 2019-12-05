c266f9bdb6b838ae4f20fa379e4a3a0a2e06af85
Piers Holt <piersholt@gmail.com>
2019-12-06 10:50:49 +1100

    wilhelm-virtual: Radio::Capabilities::UI::Config constant comments

--- lib/wilhelm/virtual/device/radio/capabilities/ui/config.rb
+++ lib/wilhelm/virtual/device/radio/capabilities/ui/config.rb
@@ -11,33 +11,41 @@ module Wilhelm
               include API
               include Constants
 
+              # 0b0000_0001
               def main_menu
                 menu_rad(state: MAIN_MENU)
               end
 
+              # 0b0000_0010
               def hide_header
                 menu_rad(state: HIDE_HEADER)
               end
 
+              # 0b0000_0100
               def hide_select
                 menu_rad(state: HIDE_SELECT)
               end
 
+              # 0b0000_1000
               def hide_tone
                 menu_rad(state: HIDE_TONE)
               end
 
+              # 0b0000_1100
               def hide_menu
                 menu_rad(state: HIDE_MENU)
               end
 
               # BM53
+              # 0b0000_0010
+              # 0b0000_1100
               def hide_overlay_ng
                 hide_header
                 hide_menu
               end
 
               # C23
+              # 0b0000_1110
               def hide_overlay
                 menu_rad(state: HIDE_OVERLAY)
               end
