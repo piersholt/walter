5998cf087d7a94babe9326743e021b1565da07c1
Piers Holt <piersholt@gmail.com>
2019-12-06 11:20:26 +1100

    wilhelm-virtual: Radio::Augmented disable basically everything...

--- lib/wilhelm/virtual/device/radio/augmented.rb
+++ lib/wilhelm/virtual/device/radio/augmented.rb
@@ -6,6 +6,7 @@ module Wilhelm
       module Radio
         # Radio::Augmented
         class Augmented < Device::Augmented
+          include Device::Capabilities::Ready
           include Wilhelm::Helpers::DataTools
           include State
           include Actions
@@ -23,34 +24,33 @@ module Wilhelm
 
             case command_id
             when PING
-              dependent = message.to
-              # logger.debug(PROC) { "Tx: Depends on: #{dependent}" }
-              depend(dependent)
+              # logger.debug(PROC) { "Tx: Depends on: #{message.to}" }
+              # depend(message.to)
             when PONG
-              # logger.debug(PROC) {Tx:  "Ready." }
+              # logger.debug(PROC) { "Tx: Ready." }
               # seen
             when TXT_MID
               # logger.debug(PROC) {Tx:  "Drawing to MID!" }
             when TXT_GT
-              logger.debug(PROC) { "Tx: TXT_GT 0x#{d2h(TXT_GT)}" }
+              # logger.debug(PROC) { "Tx: TXT_GT 0x#{d2h(TXT_GT)}" }
               # foreground
-              evaluate_display_layout(message.command)
+              # evaluate_display_layout(message.command)
             when TXT_NAV
               # logger.debug(PROC) { "Tx: TXT_NAV 0x#{d2h(TXT_NAV)}" }
               # evaluate_nav_layout(message.command)
             when CDC_REQ
               # logger.debug(PROC) { "Tx: CDC_REQ 0x#{d2h(CDC_REQ)}!" }
-              evaluate_cdc_request(message.command)
+              # evaluate_cdc_request(message.command)
             when MENU_RAD
-              logger.debug(PROC) { "Tx: MENU_RAD 0x#{d2h(MENU_RAD)}" }
+              # logger.debug(PROC) { "Tx: MENU_RAD 0x#{d2h(MENU_RAD)}" }
               # background
-              evaluate_menu_rad(message.command)
+              # evaluate_menu_rad(message.command)
             when RAD_LED
-              logger.debug(PROC) { "Tx: RAD_LED 0x#{d2h(RAD_LED)}!" }
-              evaluate_radio_led(message.command)
+              # logger.debug(PROC) { "Tx: RAD_LED 0x#{d2h(RAD_LED)}!" }
+              # evaluate_radio_led(message.command)
             when RAD_ALT
-              logger.debug(PROC) { "Tx: RAD_ALT 0x#{d2h(RAD_ALT)}" }
-              evaluate_radio_alt(message.command)
+              # logger.debug(PROC) { "Tx: RAD_ALT 0x#{d2h(RAD_ALT)}" }
+              # evaluate_radio_alt(message.command)
             end
 
             # logger.unknown(PROC) { "Handle? From: #{message.from} Command: #{message.command.h}" }
@@ -64,12 +64,12 @@ module Wilhelm
 
             case command_id
             when MFL_FUNC
-              logger.debug(PROC) { "Rx: Handling: MFL-FUNC" }
-              action_mfl_function(message)
+              # logger.debug(PROC) { "Rx: Handling: MFL-FUNC" }
+              # action_mfl_function(message)
             when BMBT_A
-              logger.debug(PROC) { "Rx: Handling: BMBT-1" }
-              handle_bmbt_1_button(message)
-              action_bmbt_1_button(message)
+              # logger.debug(PROC) { "Rx: Handling: BMBT-1" }
+              # handle_bmbt_1_button(message)
+              # action_bmbt_1_button(message)
             when BMBT_B
               # logger.debug(PROC) { "Rx: Handling BMBT-2 not implemented." }
             when SRC_CTL
@@ -78,10 +78,10 @@ module Wilhelm
             when SRC_SND
               # logger.debug(PROC) { "Rx: SRC_CTL not implemented." }
               # can get tape
-              handle_source_sound(message.command)
+              # handle_source_sound(message.command)
             when MENU_GT
-              logger.debug(PROC) { "Rx: Handling: MENU_GT"  }
-              handle_menu_gt(message.command)
+              # logger.debug(PROC) { "Rx: Handling: MENU_GT"  }
+              # handle_menu_gt(message.command)
             when INPUT
               # logger.debug(PROC) { "Rx: Handling: INPUT"  }
               # handle_tel_data(message.command)
