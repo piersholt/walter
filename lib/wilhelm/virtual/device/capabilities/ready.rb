ff90e884592a063fa412ac108f57f97867251487
Piers Holt <piersholt@gmail.com>
2019-12-13 10:13:05 +1100

    wilhelm-virtual: Device::Capabilities::Ready refactor

--- lib/wilhelm/virtual/device/capabilities/ready.rb
+++ lib/wilhelm/virtual/device/capabilities/ready.rb
@@ -8,8 +8,9 @@ module Wilhelm
         module Ready
           include API::Readiness
 
-          REPLY    = 0b0000_0000
-          ANNOUNCE = 0b0000_0001
+          DEFAULT_PRIORITY  = :glo_l
+          DEFAULT_IDENT     = 0b0000_0000
+          ANNOUNCE          = 0b0000_0001
 
           # Request
           def ping(to)
@@ -17,18 +18,18 @@ module Wilhelm
           end
 
           # Reply
-          def pong(to: :glo_l, status: REPLY)
+          def pong(to: DEFAULT_PRIORITY, status: DEFAULT_IDENT)
             p0ng(from: me, to: to, status: status)
           end
 
           # Broadcast
-          def announce(to: :glo_l, status: ANNOUNCE)
+          def announce(to: DEFAULT_PRIORITY, status: DEFAULT_IDENT | ANNOUNCE)
             return false if announced?
             p0ng(from: me, to: to, status: status)
             announced!
           end
 
-          def announce!(to: :glo_l, status: ANNOUNCE)
+          def announce!(to: DEFAULT_PRIORITY, status: DEFAULT_IDENT | ANNOUNCE)
             p0ng(from: me, to: to, status: status)
             announced!
           end
