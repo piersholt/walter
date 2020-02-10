eba2913cc7c06d17642e8139cf7b140883e2c292
Piers Holt <piersholt@gmail.com>
2020-02-10 17:16:35 +1100

    wilhelm-virtual: IKE::API::CheckControl#ccm_50

--- lib/wilhelm/virtual/device/ike/api/check_control.rb
+++ lib/wilhelm/virtual/device/ike/api/check_control.rb
@@ -9,6 +9,11 @@ module Wilhelm
           module CheckControl
             include Device::API::BaseAPI
 
+            # 0x50 CCM-50
+            def ccm_50(from: :ike, to: :ccm, arguments: [])
+              dispatch_raw_command(from, to, 0x50, arguments)
+            end
+
             # 0x52 CCM-RLY
             def ccm_relay(from: :ike, to: :ccm, **arguments)
               parse_string(arguments)
