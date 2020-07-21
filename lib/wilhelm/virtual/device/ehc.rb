a223acc810a6f9b2694cb81af6449a13b64594f2
Piers Holt <piersholt@gmail.com>
2020-07-21 18:51:10 +1000

    wilhelm-virtual: Device::EHC template

---
+++ lib/wilhelm/virtual/device/ehc.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: false
+
+# puts "\tLoading wilhelm/virtual/device/ehc"
+
+# require_relative 'ehc/api'
+# require_relative 'ehc/capabilities'
+
+# require_relative 'ehc/augmented'
+
+# AC 0F 3F A0 81 09 23 96 00 06 01 06 23 07 02 65 43
+#
+# PN: 81 09 23 96
+# HW: 00
+# CI: 06 
+# DI: 01
+# BI: 06
+# DATE: 23 07 (swong)
+# SU: 02 (Kostal)
+# SW: 65 (wrong)
